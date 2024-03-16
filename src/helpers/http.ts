1dddfe89932b23b28b45aa24d7916aaf7e1ce54a
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2024-03-16 22:11:35 +0300

    tmp: show env in console

--- src/helpers/http.ts
+++ src/helpers/http.ts
@@ -2,6 +2,8 @@ import { fetchUtils } from "react-admin";
 
 export const API_URL = import.meta.env.VITE_API_URL;
 
+console.log(import.meta.env);
+
 export const http = async (url: string, options: any = {}) => {
     if (!options.headers) {
         options.headers = new Headers({ Accept: "application/json" });
