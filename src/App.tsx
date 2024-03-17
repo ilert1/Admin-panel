1417045344df68659b960656a796d172b7596932
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2024-03-17 22:37:21 +0300

    feat: keycloak auth added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
-import { Admin, CustomRoutes, Resource, combineDataProviders, Menu, Layout, useTranslate } from "react-admin";
+import {
+    Admin,
+    CustomRoutes,
+    Resource,
+    combineDataProviders,
+    Menu,
+    Layout,
+    useTranslate,
+    AuthProvider
+} from "react-admin";
 import { TransactionDataProvider, i18nProvider } from "@/data";
 import {
     Receipt as ReceiptIcon,
@@ -6,12 +15,15 @@ import {
     AddCard as AddCardIcon,
     CreditScore as CreditScoreIcon
 } from "@mui/icons-material";
-import { BaseDataProvider, AuthProvider } from "@/data";
+import { BaseDataProvider } from "@/data";
 import { AccountList, TransactionList } from "@/components/widgets/lists";
 import { AccountCreate } from "@/components/widgets/create";
 import { AccountShow, TransactionShow } from "./components/widgets/show";
 import { Route } from "react-router-dom";
 import { PayInPage, PayOutPage } from "./pages";
+import Keycloak, { KeycloakConfig, KeycloakTokenParsed, KeycloakInitOptions } from "keycloak-js";
+import { keycloakAuthProvider } from "ra-keycloak";
+import { useEffect, useRef, useState } from "react";
 
 export const MyMenu = () => {
     const translate = useTranslate();
@@ -34,19 +46,63 @@ const dataProvider = combineDataProviders(resource => {
     }
 });
 
-export const App = () => (
-    <Admin i18nProvider={i18nProvider} dataProvider={dataProvider} authProvider={new AuthProvider()} layout={MyLayout}>
-        <Resource
-            name="accounts"
-            list={AccountList}
-            show={AccountShow}
-            create={AccountCreate}
-            icon={AccountBalanceWalletIcon}
-        />
-        <Resource name="transactions" list={TransactionList} show={TransactionShow} icon={ReceiptIcon} />
-        <CustomRoutes>
-            <Route path="/payin" element={<PayInPage />} />
-            <Route path="/payout" element={<PayOutPage />} />
-        </CustomRoutes>
-    </Admin>
-);
+const initOptions: KeycloakInitOptions = { onLoad: "login-required" };
+
+const getPermissions = (decoded: KeycloakTokenParsed) => {
+    const roles = decoded?.realm_access?.roles;
+    console.log(roles);
+    if (!roles) {
+        return false;
+    }
+    if (roles.includes("admin")) return "admin";
+    if (roles.includes("merchant")) return "merchant";
+    return false;
+};
+
+const raKeycloakOptions = {
+    onPermissions: getPermissions
+};
+
+const config: KeycloakConfig = {
+    url: import.meta.env.VITE_KEYCLOAK_URL,
+    realm: import.meta.env.VITE_KEYCLOAK_REALM,
+    clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID
+};
+
+export const App = () => {
+    const [keycloak, setKeycloak] = useState<Keycloak>();
+    const authProvider = useRef<AuthProvider>();
+
+    useEffect(() => {
+        const initKeyCloakClient = async () => {
+            const keycloakClient = new Keycloak(config);
+            await keycloakClient.init(initOptions);
+            authProvider.current = keycloakAuthProvider(keycloakClient, raKeycloakOptions);
+            setKeycloak(keycloakClient);
+        };
+        if (!keycloak) {
+            initKeyCloakClient();
+        }
+    }, [keycloak]);
+
+    return (
+        <Admin
+            i18nProvider={i18nProvider}
+            dataProvider={dataProvider}
+            authProvider={authProvider.current}
+            layout={MyLayout}>
+            <Resource
+                name="accounts"
+                list={AccountList}
+                show={AccountShow}
+                create={AccountCreate}
+                icon={AccountBalanceWalletIcon}
+            />
+            <Resource name="transactions" list={TransactionList} show={TransactionShow} icon={ReceiptIcon} />
+            <CustomRoutes>
+                <Route path="/payin" element={<PayInPage />} />
+                <Route path="/payout" element={<PayOutPage />} />
+            </CustomRoutes>
+        </Admin>
+    );
+};
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,3 @@
 export { i18nProvider } from "./i18n";
 export { BaseDataProvider } from "./base";
-export { AuthProvider } from "./auth";
 export { TransactionDataProvider } from "./transactions";
