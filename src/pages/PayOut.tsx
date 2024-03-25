05fa7328f511c5a0762ab0023cbd4dbe6f3b3e60
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2024-03-25 22:11:50 +0300

    upd: payout

--- src/pages/PayOut.tsx
+++ src/pages/PayOut.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState, ChangeEvent, useEffect } from "react";
+import { useMemo, useState, ChangeEvent } from "react";
 import { useQuery, useMutation } from "react-query";
 import {
     Card,
@@ -15,16 +15,13 @@ import {
     Button
 } from "@mui/material";
 import { useTranslate } from "react-admin";
-import { API_URL, BF_MANAGER_URL } from "@/data/base";
+import { BF_MANAGER_URL } from "@/data/base";
 
 export const PayOutPage = () => {
     const translate = useTranslate();
     const [payMethod, setPayMethod] = useState<any>("");
-    const [source, setSource] = useState<any>("");
-    const [dest, setDest] = useState<any>("");
     const [sourceValue, setSourceValue] = useState("");
     const [destValue, setDestValue] = useState("");
-    const [sourceCurrency, setSourceCurrency] = useState("");
     const [cardHolder, setCardHolder] = useState("");
     const [cardInfo, setCardInfo] = useState("");
 
@@ -36,27 +33,16 @@ export const PayOutPage = () => {
         }).then(response => response.json())
     );
 
-    const { isLoading: accountsLoading, data: accounts } = useQuery("accounts", () =>
-        fetch(`${API_URL}/accounts`, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem("access-token")}`
-            }
-        }).then(response => response.json())
-    );
-
     const { mutate: payOutCreate, isLoading: payOutCreateLoading } = useMutation(() =>
         fetch(`${BF_MANAGER_URL}/v1/payout/create`, {
             method: "POST",
             body: JSON.stringify({
                 source: {
-                    id: source.id,
                     amount: {
-                        currency: sourceCurrency,
                         value: +sourceValue
                     }
                 },
                 destination: {
-                    id: dest.id,
                     amount: {
                         currency: payMethod?.fiatCurrency,
                         value: +destValue
@@ -76,28 +62,10 @@ export const PayOutPage = () => {
         }).then(response => response.json())
     );
 
-    const isLoading = useMemo(
-        () => initialLoading || accountsLoading || payOutCreateLoading,
-        [initialLoading, accountsLoading, payOutCreateLoading]
-    );
-    const sourceAccounts = useMemo(() => accounts?.data?.filter((elem: any) => elem.type === 1), [accounts]);
-    const destinationAccounts = useMemo(() => accounts?.data?.filter((elem: any) => elem.type === 2), [accounts]);
-    const sourceCurrencies = useMemo(() => source?.amounts?.map((amount: any) => amount.currency), [source]);
+    const isLoading = useMemo(() => initialLoading || payOutCreateLoading, [initialLoading, payOutCreateLoading]);
 
     const handlePayMethodChange = (event: SelectChangeEvent) => {
         setPayMethod(event.target.value);
-    };
-
-    const handleSourceChange = (event: SelectChangeEvent) => {
-        setSource(event.target.value);
-    };
-
-    const handleDestChange = (event: SelectChangeEvent) => {
-        setDest(event.target.value);
-    };
-
-    const handleSourceCurrencyChange = (event: SelectChangeEvent) => {
-        setSourceCurrency(event.target.value);
     };
 
     const create = () => {
@@ -123,65 +91,22 @@ export const PayOutPage = () => {
                                 ))}
                             </Select>
                         </FormControl>
-                        <Stack direction="row" spacing={2}>
-                            <FormControl sx={{ m: 1, width: 300 }}>
-                                <InputLabel>{translate("pages.payOut.source")}</InputLabel>
-                                <Select value={source} onChange={handleSourceChange}>
-                                    {sourceAccounts?.map((acc: any) => (
-                                        <MenuItem key={acc.id} value={acc}>
-                                            {acc.meta.caption}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                            {sourceCurrencies?.length > 0 && (
-                                <FormControl sx={{ m: 1, width: 300 }}>
-                                    <InputLabel>{translate("pages.payOut.sourceCurrency")}</InputLabel>
-                                    <Select value={sourceCurrency} onChange={handleSourceCurrencyChange}>
-                                        {sourceCurrencies?.map((curr: any, i: number) => (
-                                            <MenuItem key={i} value={curr}>
-                                                {curr}
-                                            </MenuItem>
-                                        ))}
-                                    </Select>
-                                </FormControl>
-                            )}
-                            <TextField
-                                type="number"
-                                label={translate("pages.payOut.sourceValue")}
-                                value={sourceValue}
-                                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                                    setSourceValue(event.target.value);
-                                }}
-                            />
-                        </Stack>
-                        <Stack direction="row" spacing={2}>
-                            <FormControl sx={{ m: 1, width: 300 }}>
-                                <InputLabel>{translate("pages.payOut.destination")}</InputLabel>
-                                <Select value={dest} onChange={handleDestChange}>
-                                    {destinationAccounts?.map((acc: any) => (
-                                        <MenuItem key={acc.id} value={acc}>
-                                            {acc.meta.caption}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            </FormControl>
-                            {payMethod && (
-                                <TextField
-                                    label={translate("pages.payOut.destinationCurrency")}
-                                    value={payMethod?.fiatCurrency}
-                                    disabled
-                                />
-                            )}
-                            <TextField
-                                type="number"
-                                label={translate("pages.payOut.destValue")}
-                                value={destValue}
-                                onChange={(event: ChangeEvent<HTMLInputElement>) => {
-                                    setDestValue(event.target.value);
-                                }}
-                            />
-                        </Stack>
+                        <TextField
+                            type="number"
+                            label={translate("pages.payOut.sourceValue")}
+                            value={sourceValue}
+                            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                setSourceValue(event.target.value);
+                            }}
+                        />
+                        <TextField
+                            type="number"
+                            label={translate("pages.payOut.destValue")}
+                            value={destValue}
+                            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                setDestValue(event.target.value);
+                            }}
+                        />
                         <TextField
                             sx={{ m: 1, width: 300 }}
                             label={translate("pages.payOut.cardHolder")}
@@ -200,16 +125,7 @@ export const PayOutPage = () => {
                             }}
                         />
                         <Button
-                            disabled={
-                                !payMethod ||
-                                !source ||
-                                !sourceCurrency ||
-                                !sourceValue ||
-                                !dest ||
-                                !destValue ||
-                                !cardInfo ||
-                                !cardHolder
-                            }
+                            disabled={!payMethod || !sourceValue || !destValue || !cardInfo || !cardHolder}
                             onClick={create}>
                             {translate("pages.payOut.create")}
                         </Button>
