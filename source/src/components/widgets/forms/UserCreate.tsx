25a21685339a9caac55cb00c562b474ceb73468f
Humming_ubuntu <hduser@humming>
2024-08-19 13:07:12 -0700

    fixing ssr after review

--- source/src/components/widgets/forms/UserCreate.tsx
+++ source/src/components/widgets/forms/UserCreate.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
