c3064322d2c2f99d242592b4ed1d8afa46d5212e
GrigDavid <MyEmail>
2024-08-21 08:03:25 +0300

    fix: merge conflicts fix

---
+++ source/src/components/widgets/create/UserCreate.tsx
@@ -0,0 +1,48 @@
+import { useNavigate } from "react-router-dom";
+import { UserCreateForm } from "../forms";
+import { toast } from "sonner";
+import { CreateContextProvider, useCreateController, useTranslate } from "react-admin";
+
+const UserCreate = () => {
+    const translate = useTranslate();
+    const navigate = useNavigate();
+    const contrProps = useCreateController();
+
+    const onSuccess = (data: any) => {
+        toast.success(translate("resources.users.create.success"), {
+            dismissible: true,
+            duration: 3000,
+            description: translate("resources.users.create.successMessage")
+        });
+        navigate(`/${contrProps.resource}/${data.id}/show`);
+    };
+
+    const onError = () => {
+        toast.error(translate("resources.users.create.error"), {
+            dismissible: true,
+            duration: 3000,
+            description: translate("resources.users.create.errorMessage")
+        });
+    };
+
+    const createUserRecord = (data: any) => {
+        if (contrProps.save !== undefined) {
+            contrProps.save(data, { onSuccess, onError });
+        }
+    };
+
+    return (
+        <CreateContextProvider value={contrProps}>
+            {contrProps.save !== undefined ? (
+                <UserCreateForm
+                    onSubmit={createUserRecord}
+                    isDisabled={contrProps.saving !== undefined ? contrProps.saving : false}
+                />
+            ) : (
+                <>{navigate(`/${contrProps.resource}`)}</>
+            )}
+        </CreateContextProvider>
+    );
+};
+
+export default UserCreate;
