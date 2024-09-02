72ef64e607fc75f726e70575e4a9258d051c3aac
Konstantin <tuskancik226@gmail.com>
2024-09-02 15:56:45 +0300

    feat: change date-picker to new design

--- source/src/components/ui/date-picker.tsx
+++ source/src/components/ui/date-picker.tsx
@@ -1,7 +1,6 @@
 import { CalendarIcon } from "lucide-react";
 import { format } from "date-fns";
 
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
@@ -25,9 +24,9 @@ export function DatePicker(props: { placeholder: string; date?: Date; onChange?:
     return (
         <Popover>
             <PopoverTrigger asChild>
-                <Button variant={"outline"} className={cn(!date && "text-muted-foreground")}>
-                    <CalendarIcon className="mr-2 h-4 w-4" />
+                <Button variant={"outline"} className={"flex items-center gap-3 text-neutral-100"}>
                     {date ? format(date, "dd.MM.yyyy") : <span>{props.placeholder}</span>}
+                    <CalendarIcon className="h-4 w-4 text-green-50 mb-0.5" />
                 </Button>
             </PopoverTrigger>
             <PopoverContent className="w-auto p-0" align="start">
