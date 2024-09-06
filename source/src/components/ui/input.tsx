52f45d4e73fd62ca587461d13eaa320bea5ea976
GrigDavid <MyEmail>
2024-09-06 12:22:46 +0300

    feat: New input component

--- source/src/components/ui/input.tsx
+++ source/src/components/ui/input.tsx
@@ -1,22 +1,89 @@
+import { X, Eye, EyeOff } from "lucide-react";
 import * as React from "react";
-
 import { cn } from "@/lib/utils";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
-    return (
-        <input
-            type={type}
-            className={cn(
-                "flex h-10 w-full rounded-4 text-neutral-80 border border-neutral-40 hover:border-green-50 active:border-green-50 focus:border-green-50 bg-neutral-0 px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-60 focus:outline-none disabled:border-neutral-40 disabled:bg-neutral-40 transition duration-200",
-                className
-            )}
-            ref={ref}
-            {...props}
-        />
-    );
-});
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+    ({ className, type, value, onChange, disabled, ...props }, ref) => {
+        const [inputValue, setInputValue] = React.useState<string | number | readonly string[] | undefined>(
+            value || ""
+        );
+        const [showPassword, setShowPassword] = React.useState(false);
+        const [isFocused, setIsFocused] = React.useState(false);
+
+        const handleClear = () => {
+            setInputValue("");
+            if (onChange) {
+                const event = { target: { value: "" } } as React.ChangeEvent<HTMLInputElement>;
+                onChange(event);
+            }
+        };
+
+        const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+            setInputValue(e.target.value);
+            if (onChange) {
+                onChange(e);
+            }
+        };
+
+        return (
+            <div className="relative flex items-center w-full">
+                <input
+                    type={type === "password" && showPassword ? "text" : type}
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    onFocus={() => setIsFocused(true)}
+                    onBlur={() => setIsFocused(false)}
+                    disabled={disabled}
+                    className={cn(
+                        "flex h-10 w-full rounded-4 text-neutral-80 border border-neutral-40 hover:border-green-50 active:border-green-50 focus:border-green-50 bg-neutral-0 px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-60 focus:outline-none disabled:border-neutral-40 disabled:bg-neutral-40 transition duration-200",
+                        className,
+                        inputValue || type === "password" ? "pr-12" : "pr-10"
+                    )}
+                    ref={ref}
+                    {...props}
+                />
+                {inputValue && (
+                    <button
+                        type="button"
+                        onClick={handleClear}
+                        className={cn(
+                            "absolute inset-y-0",
+                            type === "password" ? "right-9" : "right-3",
+                            "flex items-center justify-center text-neutral-60 hover:text-neutral-80"
+                        )}>
+                        <X className="w-4 h-4" />
+                    </button>
+                )}
+                {type === "password" && (
+                    <button
+                        type="button"
+                        onClick={() => setShowPassword(!showPassword)}
+                        disabled={disabled}
+                        className="absolute right-3 top-0 bottom-0 flex items-center justify-center">
+                        {showPassword ? (
+                            <EyeOff
+                                className={cn(
+                                    "h-5 w-5",
+                                    disabled ? "text-neutral-60" : isFocused ? "text-green-50" : "text-green-40"
+                                )}
+                            />
+                        ) : (
+                            <Eye
+                                className={cn(
+                                    "h-5 w-5",
+                                    disabled ? "text-neutral-60" : isFocused ? "text-green-50" : "text-green-40"
+                                )}
+                            />
+                        )}
+                    </button>
+                )}
+            </div>
+        );
+    }
+);
+
 Input.displayName = "Input";
 
 export { Input };
--- source/src/pages/LoginPage.tsx
+++ source/src/pages/LoginPage.tsx
@@ -37,15 +37,15 @@ export const LoginPage = () => {
                 backgroundRepeat: "no-repeat"
             }}>
             <div
-                className={`w-full max-w-md px-16 pb-16 rounded-16 shadow-md ${
+                className={`mx-4 w-full max-w-md px-8 sm:px-16 pb-16 rounded-16 shadow-md min-w-[240px] z-10 ${
                     theme === "dark" ? "bg-neutral-0" : "bg-neutral-100"
                 }`}>
-                <div className="flex justify-center mb-6 mt-5">
-                    <img src="/MoneyGateLogo.svg" alt="Logo" className="h-20 w-56" />
+                <div className="flex justify-center mb-2.5 mt-5">
+                    <img src="/MoneyGateLogo.svg" alt="Logo" className="h-20 w-56 pointer-events-none select-none" />
                 </div>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-5">
-                        <label htmlFor="username" className="block text-note-1 text-neutral-30">
+                        <label htmlFor="username" className="block text-note-1 text-neutral-30 mb-1">
                             {translate("app.login.usernameOrEmail")}
                         </label>
                         <Input
@@ -53,11 +53,11 @@ export const LoginPage = () => {
                             type="text"
                             value={username}
                             onChange={handleChange(setUsername)}
-                            className="mt-1 block w-full border-gray-300 rounded-md shadow-sm text-title-1"
+                            className=" block w-full border-gray-300 rounded-md shadow-sm text-title-1"
                         />
                     </div>
                     <div className="mb-5 relative">
-                        <label htmlFor="password" className="block text-note-1 text-neutral-30">
+                        <label htmlFor="password" className="block text-note-1 text-neutral-30 mb-1">
                             {translate("app.login.password")}
                         </label>
                         <div className="flex items-center relative">
@@ -66,18 +66,8 @@ export const LoginPage = () => {
                                 type={showPassword ? "text" : "password"}
                                 value={password}
                                 onChange={handleChange(setPassword)}
-                                className="mt-1 block w-full border-gray-300 rounded-md shadow-sm pr-10 text-title-1"
+                                className=" block w-full border-gray-300 rounded-md shadow-sm pr-10 text-title-1"
                             />
-                            <button
-                                type="button"
-                                onClick={() => setShowPassword(!showPassword)}
-                                className="absolute inset-y-0 right-0 pr-3 flex items-center mt-1">
-                                {showPassword ? (
-                                    <EyeOff className="h-5 w-5 text-green-40" />
-                                ) : (
-                                    <Eye className="h-5 w-5 text-green-40" />
-                                )}
-                            </button>
                         </div>
                     </div>
                     <Button type="submit" color="primary" variant="default" className="w-full">
@@ -90,7 +80,7 @@ export const LoginPage = () => {
                 <img
                     src="/BlowFish.svg"
                     alt="Decorative"
-                    className="w-[400px] h-[400px] invisible md:visible md:w-[200px] md:h-[200px] lg:w-[320px] lg:h-[320px]"
+                    className="w-[200px] h-[200px] lg:w-[320px] lg:h-[320px] xl:w-[400px] xl:h-[400px]pointer-events-none select-none -z-50"
                 />
             </div>
         </div>
