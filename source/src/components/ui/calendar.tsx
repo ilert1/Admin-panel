21a4e75ae33d7b3f5122f1fad97fc33cddda6c9e
Konstantin <tuskancik226@gmail.com>
2024-09-25 11:37:23 +0300

    feat: complete stilization for light and dark theme

--- source/src/components/ui/calendar.tsx
+++ source/src/components/ui/calendar.tsx
@@ -12,36 +12,39 @@ function Calendar({ className, classNames, ...props }: DayPickerProps) {
             captionLayout="dropdown"
             startMonth={new Date(2014, 0)}
             hideWeekdays
-            className={cn("p-4 rounded-4 bg-neutral-0 border-green-50 border", className)}
+            className={cn(
+                "p-4 pb-2 rounded-4 bg-neutral-0 dark:border-green-50 border-green-60 border shadow",
+                className
+            )}
             classNames={{
                 months: "flex flex-col sm:flex-row space-y-4 sm:space-y-0",
-                month_caption: "flex justify-center items-center [&_svg]:hidden text-green-40",
+                month_caption: "flex justify-center items-center [&_svg]:hidden dark:text-green-40 text-green-50",
                 months_dropdown: "bg-neutral-0 text-neutral-100 border-green-50 border",
                 dropdowns: "static flex items-center gap-1",
                 caption_label: "flex justify-center text-green-40 items-center",
                 nav: "flex",
                 button_previous: cn(
                     buttonVariants({ variant: "textBtn" }),
-                    "h-5 w-5 bg-transparent p-0 opacity-50 hover:opacity-100 font-bold absolute left-4 top-5 z-10"
+                    "h-5 w-5 bg-transparent p-0 opacity-50 hover:opacity-100 font-bold absolute left-4 top-[22px] z-10"
                 ),
                 button_next: cn(
                     buttonVariants({ variant: "textBtn" }),
-                    "h-5 w-5 bg-transparent p-0 opacity-50 hover:opacity-100 font-bold absolute right-4 top-5 z-10"
+                    "h-5 w-5 bg-transparent p-0 opacity-50 hover:opacity-100 font-bold absolute right-4 top-[22px] z-10"
                 ),
                 selected:
                     "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-                day: "h-5 w-5 p-0 font-normal text-sm text-neutral-10",
+                day: "p-0 text-base font-normal dark:text-neutral-90 text-neutral-80 [&.day-range-end]:text-neutral-0 [&.day-range-start]:text-neutral-0 dark:[&.day-range-end]:text-neutral-100 dark:[&.day-range-start]:text-neutral-100",
                 day_hidden: "invisible",
                 day_outside: "text-muted-foreground opacity-50",
                 day_disabled: "text-muted-foreground opacity-50",
-                day_button: "h-5 w-5 m-2",
-                today: "relative z-10 [&:not([aria-selected])]:before:absolute [&:not([aria-selected])]:before:outline [&:not([aria-selected])]:before:outline-1 [&:not([aria-selected])]:before:outline-green-40 [&:not([aria-selected])]:before:rounded-4 [&:not([aria-selected])]:before:top-1 [&:not([aria-selected])]:before:bottom-1 [&:not([aria-selected])]:before:left-1 [&:not([aria-selected])]:before:right-1 [&:not([aria-selected])]:before:-z-10",
+                day_button: "w-5 h-5 m-2 text-base font-normal flex items-center justify-center",
+                today: "relative z-10 [&:not([aria-selected])]:before:absolute [&:not([aria-selected])]:before:outline [&:not([aria-selected])]:before:outline-1 dark:[&:not([aria-selected])]:before:outline-green-40 [&:not([aria-selected])]:before:outline-green-50 [&:not([aria-selected])]:before:rounded-4 [&:not([aria-selected])]:before:top-1 [&:not([aria-selected])]:before:bottom-1 [&:not([aria-selected])]:before:left-1 [&:not([aria-selected])]:before:right-1 [&:not([aria-selected])]:before:-z-10",
                 range_middle:
-                    "relative after:absolute after:bg-green-20 after:top-1 after:bottom-1 after:left-0 after:right-0 after:-z-10 z-10",
+                    "relative after:absolute dark:after:bg-green-20 after:bg-green-0 after:top-1 after:bottom-1 after:left-0 after:right-0 after:-z-10 z-10",
                 range_start:
-                    "relative z-10 after:absolute after:bg-green-50 after:rounded-4 after:top-1 after:bottom-1 after:left-1 after:right-1 after:-z-10 before:absolute before:bg-green-20 before:top-1 before:bottom-1 before:left-3 before:right-0 before:-z-10",
+                    "relative z-10 after:absolute after:bg-green-50 after:rounded-4 after:top-1 after:bottom-1 after:left-1 after:right-1 after:-z-10 before:absolute dark:before:bg-green-20 before:bg-green-0 before:top-1 before:bottom-1 before:left-3 before:right-0 before:-z-10 day-range-start",
                 range_end:
-                    "relative z-10 after:absolute after:bg-green-50 after:rounded-4 after:top-1 after:bottom-1 after:left-1 after:right-1 after:-z-10 before:absolute before:bg-green-20 before:top-1 before:bottom-1 before:left-0 before:right-3 before:-z-10",
+                    "relative z-10 after:absolute after:bg-green-50 after:rounded-4 after:top-1 after:bottom-1 after:left-1 after:right-1 after:-z-10 before:absolute dark:before:bg-green-20 before:bg-green-0 before:top-1 before:bottom-1 before:left-0 before:right-3 before:-z-10 day-range-end",
                 ...classNames
             }}
             components={{
@@ -70,8 +73,8 @@ function Calendar({ className, classNames, ...props }: DayPickerProps) {
                                 id={`${option.value}`}
                                 onClick={() => handleChange(String(option.value))}
                                 className={
-                                    "text-sm font-medium text-neutral-100 rounded-4 px-1 py-1.5 pointer" +
-                                    (option.value === value ? " bg-green-50" : "")
+                                    "text-sm font-medium dark:text-neutral-90 text-neutral-80 rounded-4 px-1 py-1.5 pointer" +
+                                    (option.value === value ? " bg-green-50 dark:text-neutral-90 text-neutral-0" : "")
                                 }
                                 key={`${option.value}-${id}`}>
                                 {option.label}
@@ -88,7 +91,7 @@ function Calendar({ className, classNames, ...props }: DayPickerProps) {
                     return (
                         <div>
                             <button
-                                className="pointer py-1 px-1.5 text-sm font-medium"
+                                className="pointer px-4 py-1.5 text-sm font-medium"
                                 onClick={() => setShowDropdown(true)}>
                                 {selected?.label}
                             </button>
