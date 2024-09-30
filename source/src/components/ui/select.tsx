24cef91df07d28fb9e02695f66cc920bc0da3c45
Konstantin <tuskancik226@gmail.com>
2024-09-30 15:23:32 +0300

    feat: continue restyle select component

--- source/src/components/ui/select.tsx
+++ source/src/components/ui/select.tsx
@@ -13,47 +13,24 @@ const SelectValue = SelectPrimitive.Value;
 const SelectTrigger = React.forwardRef<
     React.ElementRef<typeof SelectPrimitive.Trigger>,
     React.ComponentPropsWithoutRef<typeof SelectPrimitive.Trigger>
->(({ className, children, ...props }, ref) => (
-    <SelectPrimitive.Trigger
-        ref={ref}
-        className={cn(
-            "flex h-9 w-full items-center justify-between border border-neutral-40 rounded-4 hover:border-green-20 bg-neutral-0 px-3 py-2 text-sm ring-offset-background placeholder:text-neutral-80 focus:border-green-50 active:border-green-50 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 focus:outline-none",
-            className
-        )}
-        {...props}>
-        {children}
-        <SelectPrimitive.Icon asChild>
-            <ChevronDown className="h-4 w-4 opacity-50 text-green-50" />
-        </SelectPrimitive.Icon>
-    </SelectPrimitive.Trigger>
-));
+>(({ className, children, ...props }, ref) => {
+    console.log("open", children);
+    return (
+        <SelectPrimitive.Trigger
+            ref={ref}
+            className={cn(
+                "flex h-9 w-full items-center justify-between border border-neutral-40 rounded-4 hover:border-green-20 bg-neutral-0 px-3 py-2 text-sm ring-offset-background [&:is([data-state='open'])]:border-green-50 active:border-green-50 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 focus:outline-none [&:not([data-placeholder])]:dark:text-neutral-40 dark:text-neutral-70 text-neutral-80 [&:is([data-state='open'])>svg]:rotate-180",
+                className
+            )}
+            {...props}>
+            {children}
+            <SelectPrimitive.Icon asChild>
+                <ChevronDown className="h-4 w-4 text-green-50 dark:text-green-40 transition-transform" />
+            </SelectPrimitive.Icon>
+        </SelectPrimitive.Trigger>
+    );
+});
 SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
-
-const SelectScrollUpButton = React.forwardRef<
-    React.ElementRef<typeof SelectPrimitive.ScrollUpButton>,
-    React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollUpButton>
->(({ className, ...props }, ref) => (
-    <SelectPrimitive.ScrollUpButton
-        ref={ref}
-        className={cn("flex cursor-default items-center justify-center py-1", className)}
-        {...props}>
-        <ChevronUp className="h-4 w-4" />
-    </SelectPrimitive.ScrollUpButton>
-));
-SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName;
-
-const SelectScrollDownButton = React.forwardRef<
-    React.ElementRef<typeof SelectPrimitive.ScrollDownButton>,
-    React.ComponentPropsWithoutRef<typeof SelectPrimitive.ScrollDownButton>
->(({ className, ...props }, ref) => (
-    <SelectPrimitive.ScrollDownButton
-        ref={ref}
-        className={cn("flex cursor-default items-center justify-center py-1", className)}
-        {...props}>
-        <ChevronDown className="h-4 w-4" />
-    </SelectPrimitive.ScrollDownButton>
-));
-SelectScrollDownButton.displayName = SelectPrimitive.ScrollDownButton.displayName;
 
 const SelectContent = React.forwardRef<
     React.ElementRef<typeof SelectPrimitive.Content>,
@@ -63,14 +40,13 @@ const SelectContent = React.forwardRef<
         <SelectPrimitive.Content
             ref={ref}
             className={cn(
-                "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border border-green-50 dark:border-neutral-50 bg-white dark:bg-green-60 text-neutral-80 shadow-1 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+                "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border border-green-50 dark:border-neutral-50 bg-white dark:bg-green-60 shadow-1 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
                 position === "popper" &&
                     "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
                 className
             )}
             position={position}
             {...props}>
-            <SelectScrollUpButton />
             <SelectPrimitive.Viewport
                 className={cn(
                     "p-0",
@@ -79,7 +55,6 @@ const SelectContent = React.forwardRef<
                 )}>
                 {children}
             </SelectPrimitive.Viewport>
-            <SelectScrollDownButton />
         </SelectPrimitive.Content>
     </SelectPrimitive.Portal>
 ));
@@ -123,15 +98,4 @@ const SelectSeparator = React.forwardRef<
 ));
 SelectSeparator.displayName = SelectPrimitive.Separator.displayName;
 
-export {
-    Select,
-    SelectGroup,
-    SelectValue,
-    SelectTrigger,
-    SelectContent,
-    SelectLabel,
-    SelectItem,
-    SelectSeparator,
-    SelectScrollUpButton,
-    SelectScrollDownButton
-};
+export { Select, SelectGroup, SelectValue, SelectTrigger, SelectContent, SelectLabel, SelectItem, SelectSeparator };
