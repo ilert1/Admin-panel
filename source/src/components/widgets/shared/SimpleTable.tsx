58255f415ed41b3003edd80a4a93b0b8685405ab
Konstantin <tuskancik226@gmail.com>
2024-09-30 10:31:22 +0300

    fix: remove unused div blocks

--- source/src/components/widgets/shared/SimpleTable.tsx
+++ source/src/components/widgets/shared/SimpleTable.tsx
@@ -1,4 +1,4 @@
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable, getPaginationRowModel } from "@tanstack/react-table";
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 interface SimpleTableProps<TData, TValue> {
@@ -26,58 +26,56 @@ export function SimpleTable<TData, TValue>({
     });
 
     return (
-        <div className="rounded-md border">
-            <Table>
-                <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup, i) => (
-                        <TableRow key={i}>
-                            {headerGroup.headers.map((header, j) => {
-                                return (
-                                    <TableHead
-                                        key={j}
-                                        className={
-                                            tableType === TableTypes.COLORED
-                                                ? "bg-green-50 text-neutral-100 border border-x border-muted"
-                                                : ""
-                                        }>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(header.column.columnDef.header, header.getContext())}
-                                    </TableHead>
-                                );
-                            })}
+        <Table>
+            <TableHeader>
+                {table.getHeaderGroups().map((headerGroup, i) => (
+                    <TableRow key={i}>
+                        {headerGroup.headers.map((header, j) => {
+                            return (
+                                <TableHead
+                                    key={j}
+                                    className={
+                                        tableType === TableTypes.COLORED
+                                            ? "bg-green-50 text-neutral-100 border border-x border-muted"
+                                            : ""
+                                    }>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(header.column.columnDef.header, header.getContext())}
+                                </TableHead>
+                            );
+                        })}
+                    </TableRow>
+                ))}
+            </TableHeader>
+            <TableBody>
+                {table.getRowModel().rows?.length ? (
+                    table.getRowModel().rows.map((row, i) => (
+                        <TableRow
+                            key={i}
+                            data-state={row.getIsSelected() && "selected"}
+                            className={
+                                tableType === TableTypes.COLORED
+                                    ? "bg-neutral-0 text-neutral-100 border border-muted"
+                                    : ""
+                            }>
+                            {row.getVisibleCells().map((cell, j) => (
+                                <TableCell
+                                    key={j}
+                                    className={tableType === TableTypes.COLORED ? "border border-muted" : ""}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </TableCell>
+                            ))}
                         </TableRow>
-                    ))}
-                </TableHeader>
-                <TableBody>
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows.map((row, i) => (
-                            <TableRow
-                                key={i}
-                                data-state={row.getIsSelected() && "selected"}
-                                className={
-                                    tableType === TableTypes.COLORED
-                                        ? "bg-neutral-0 text-neutral-100 border border-muted"
-                                        : ""
-                                }>
-                                {row.getVisibleCells().map((cell, j) => (
-                                    <TableCell
-                                        key={j}
-                                        className={tableType === TableTypes.COLORED ? "border border-muted" : ""}>
-                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        ))
-                    ) : (
-                        <TableRow>
-                            <TableCell colSpan={columns.length} className="h-24 text-center">
-                                No results.
-                            </TableCell>
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
-        </div>
+                    ))
+                ) : (
+                    <TableRow>
+                        <TableCell colSpan={columns.length} className="h-24 text-center">
+                            No results.
+                        </TableCell>
+                    </TableRow>
+                )}
+            </TableBody>
+        </Table>
     );
 }
