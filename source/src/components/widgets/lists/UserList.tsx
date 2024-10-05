f2331d52d47b61423b67c2dc919b488a891db73e
zulus-ru <maxsaxmax@mail.ru>
2024-10-05 11:53:30 +0300

    fixed button user-select

--- source/src/components/widgets/lists/UserList.tsx
+++ source/src/components/widgets/lists/UserList.tsx
@@ -121,7 +121,7 @@ const UserFilterSidebar = () => {
 
                 <Dialog>
                     <DialogTrigger>
-                        <div className="text-white dark:text-neutral-100 whitespace-nowrap rounded-4 text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 duration-200 h-9 px-4 py-2 bg-green-50 hover:bg-green-40 active:bg-green-20 disabled:bg-neutral-20 disabled:text-neutral-80 flex items-center justify-center gap-1 font-normal">
+                        <div className="text-white dark:text-neutral-100 whitespace-nowrap rounded-4 text-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 duration-200 h-9 px-4 py-2 bg-green-50 hover:bg-green-40 active:bg-green-20 disabled:bg-neutral-20 disabled:text-neutral-80 flex items-center justify-center gap-1 font-normal select-none">
                             <CirclePlus width={16} height={16} />
                             <span>{translate("resources.users.createButton")}</span>
                         </div>
