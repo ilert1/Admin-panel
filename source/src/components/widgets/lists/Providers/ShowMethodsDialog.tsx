7d32f890ce7bc39dff4cc2e0b1cf84c287a45fa1
David <email@mail.com>
2024-10-21 12:47:03 +0400

    fix: Removed console.log, methods readonly

--- source/src/components/widgets/lists/Providers/ShowMethodsDialog.tsx
+++ source/src/components/widgets/lists/Providers/ShowMethodsDialog.tsx
@@ -48,6 +48,7 @@ export const ShowMethodsDialog = (props: ShowMethodDialogProps) => {
                             setCode={() => {}}
                             onErrorsChange={() => {}}
                             onValidChange={() => {}}
+                            disabled
                         />
                     </div>
                 </DialogHeader>
