bc602f403c6f199489be0dc0f1b8e366b7b539ea
David <email@mail.com>
2024-11-04 14:41:09 +0400

    feat: Added reusable LangSwitcher component

--- source/src/pages/LoginPage.tsx
+++ source/src/pages/LoginPage.tsx
@@ -4,6 +4,7 @@ import { useSearchParams, useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { useTheme } from "@/components/providers";
 import { Input } from "@/components/ui/input";
+import { LangSwitcher } from "@/components/widgets/components/LangSwitcher";
 
 const realm = import.meta.env.VITE_KEYCLOAK_REALM;
 const kk = import.meta.env.VITE_KEYCLOAK_URL;
@@ -13,11 +14,13 @@ export const LoginPage = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [totpCode, setTotpCode] = useState("");
+
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [showPassword, setShowPassword] = useState(false);
     const [error, setError] = useState("");
     const [formEnabled, setFormEnabled] = useState(true);
     const { theme } = useTheme();
+
     const translate = useTranslate();
     const login = useLogin();
     const navigate = useNavigate();
@@ -82,77 +85,86 @@ export const LoginPage = () => {
         };
 
     return (
-        <div
-            className="relative flex items-center justify-center min-h-screen bg-loginBG overflow-hidden"
-            style={{
-                backgroundImage: "url(/LoginBackground.svg)",
-                backgroundSize: "cover",
-                backgroundRepeat: "no-repeat"
-            }}>
+        <>
+            <div className="flex flex-shrink-0 justify-end h-[84px] items-center gap-4 bg-header px-4 relative z-100 pointer-events-auto z">
+                <LangSwitcher />
+            </div>
             <div
-                className={`mx-4 w-full max-w-md px-8 sm:px-16 pb-16 rounded-16 shadow-md min-w-[240px] z-10 ${
-                    theme === "dark" ? "bg-neutral-0" : "bg-neutral-100"
-                }`}>
-                <div className="flex justify-center mb-2.5 mt-5">
-                    <img src="/MoneyGateLogo.svg" alt="Logo" className="h-20 w-56 pointer-events-none select-none" />
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-5">
-                        <label htmlFor="username" className="block text-note-1 text-neutral-30 mb-1">
-                            {translate("app.login.usernameOrEmail")}
-                        </label>
-                        <Input
-                            id="username"
-                            type="text"
-                            value={username}
-                            onChange={handleChange(setUsername)}
-                            className=" block w-full border-gray-300 rounded-md shadow-sm text-title-1"
+                className="relative flex items-center justify-center bg-loginBG overflow-hidden h-[calc(100vh-84px)]"
+                style={{
+                    backgroundImage: "url(/LoginBackground.svg)",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat"
+                }}>
+                <div
+                    className={`mx-4 w-full max-w-md px-8 sm:px-16 pb-16 rounded-16 shadow-md min-w-[240px] z-10 ${
+                        theme === "dark" ? "bg-neutral-0" : "bg-neutral-100"
+                    }`}>
+                    <div className="flex justify-center mb-2.5 mt-5">
+                        <img
+                            src="/MoneyGateLogo.svg"
+                            alt="Logo"
+                            className="h-20 w-56 pointer-events-none select-none"
                         />
                     </div>
-                    <div className="mb-5 relative">
-                        <label htmlFor="password" className="block text-note-1 text-neutral-30 mb-1">
-                            {translate("app.login.password")}
-                        </label>
-                        <div className="flex items-center relative">
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-5">
+                            <label htmlFor="username" className="block text-note-1 text-neutral-30 mb-1">
+                                {translate("app.login.usernameOrEmail")}
+                            </label>
                             <Input
-                                id="password"
-                                type={showPassword ? "text" : "password"}
-                                value={password}
-                                onChange={handleChange(setPassword)}
-                                className=" block w-full border-gray-300 rounded-md shadow-sm pr-10 text-title-1"
+                                id="username"
+                                type="text"
+                                value={username}
+                                onChange={handleChange(setUsername)}
+                                className=" block w-full border-gray-300 rounded-md shadow-sm text-title-1"
                             />
                         </div>
-                    </div>
-                    <div className="mb-5">
-                        <label htmlFor="username" className="block text-note-1 text-neutral-30 mb-1">
-                            {translate("app.login.totp")}
-                        </label>
-                        <Input
-                            id="totp"
-                            type="text"
-                            value={totpCode}
-                            onChange={handleChange(setTotpCode)}
-                            className=" block w-full border-gray-300 rounded-md shadow-sm text-title-1"
-                        />
-                        <div className="flex justify-center mt-4">
-                            <a className="text-xs text-green-50 dark:text-green-40" href={configure2faLink}>
-                                {translate("app.login.configure2fa")}
-                            </a>
+                        <div className="mb-5 relative">
+                            <label htmlFor="password" className="block text-note-1 text-neutral-30 mb-1">
+                                {translate("app.login.password")}
+                            </label>
+                            <div className="flex items-center relative">
+                                <Input
+                                    id="password"
+                                    type={showPassword ? "text" : "password"}
+                                    value={password}
+                                    onChange={handleChange(setPassword)}
+                                    className=" block w-full border-gray-300 rounded-md shadow-sm pr-10 text-title-1"
+                                />
+                            </div>
                         </div>
-                    </div>
-                    <Button type="submit" color="primary" variant="default" className="w-full">
-                        {translate("app.login.login")}
-                    </Button>
-                    {error && <div className="text-red-30 text-note-1 mt-5">{error}</div>}
-                </form>
+                        <div className="mb-5">
+                            <label htmlFor="username" className="block text-note-1 text-neutral-30 mb-1">
+                                {translate("app.login.totp")}
+                            </label>
+                            <Input
+                                id="totp"
+                                type="text"
+                                value={totpCode}
+                                onChange={handleChange(setTotpCode)}
+                                className=" block w-full border-gray-300 rounded-md shadow-sm text-title-1"
+                            />
+                            <div className="flex justify-center mt-4">
+                                <a className="text-xs text-green-50 dark:text-green-40" href={configure2faLink}>
+                                    {translate("app.login.configure2fa")}
+                                </a>
+                            </div>
+                        </div>
+                        <Button type="submit" color="primary" variant="default" className="w-full">
+                            {translate("app.login.login")}
+                        </Button>
+                        {error && <div className="text-red-30 text-note-1 mt-5">{error}</div>}
+                    </form>
+                </div>
+                <div className="absolute bottom-[-20px] right-[-20px] p-4">
+                    <img
+                        src="/BlowFish.svg"
+                        alt="Decorative"
+                        className="w-[200px] h-[200px] lg:w-[320px] lg:h-[320px] xl:w-[400px] xl:h-[400px]pointer-events-none select-none -z-50"
+                    />
+                </div>
             </div>
-            <div className="absolute bottom-[-20px] right-[-20px] p-4">
-                <img
-                    src="/BlowFish.svg"
-                    alt="Decorative"
-                    className="w-[200px] h-[200px] lg:w-[320px] lg:h-[320px] xl:w-[400px] xl:h-[400px]pointer-events-none select-none -z-50"
-                />
-            </div>
-        </div>
+        </>
     );
 };
