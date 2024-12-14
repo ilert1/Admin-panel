b7e8a377d158f75e99d9903e0c92884dea02cdf9
GrigDavid <MyEmail>
2024-12-14 15:58:12 +0400

    fix: Removed default img icon

--- source/src/components/widgets/shared/Icon.tsx
+++ source/src/components/widgets/shared/Icon.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+
 export function Icon({ name, folder = "" }: { name: string; folder?: string }) {
-    return <img src={(folder && `/${folder}/`) + name + ".svg"} alt={name} />;
+    const [isImageError, setIsImageError] = useState(false);
+
+    if (isImageError) {
+        return <span>{name}</span>;
+    }
+
+    return (
+        <img
+            src={(folder && `/${folder}/`) + name + ".svg"}
+            alt={name}
+            onError={e => {
+                e.currentTarget.style.display = "none";
+                setIsImageError(true);
+            }}
+        />
+    );
 }
