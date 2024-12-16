b6be9a46071d568f51811702fe8e9692d22c1e6c
Konstantin <tuskancik226@gmail.com>
2024-12-16 15:03:26 +0300

    fix: remove unused code from account sheet

--- source/src/components/widgets/lists/Accounts/ShowAccountSheet.tsx
+++ source/src/components/widgets/lists/Accounts/ShowAccountSheet.tsx
@@ -21,39 +21,35 @@ export const ShowAccountSheet: React.FC<ShowSheetProps> = ({
     const translate = useTranslate();
 
     return (
-        <>
-            <Sheet onOpenChange={onOpenChange} open={open}>
-                <SheetContent
-                    className="sm:max-w-[1015px] !max-h-[calc(100dvh-84px)] w-full p-0 m-0 top-[84px] flex flex-col border-0"
-                    tabIndex={-1}
-                    style={{ backgroundColor: "rgba(19, 35, 44, 1)" }}
-                    close={false}>
-                    <SheetHeader className="p-[42px] pb-0 flex-shrink-0">
-                        <div className="flex flex-col gap-2">
-                            <div className="flex justify-between items-center ">
-                                <SheetTitle className="!text-display-1">
-                                    {translate("app.ui.accountHistory")}
-                                </SheetTitle>
+        <Sheet onOpenChange={onOpenChange} open={open}>
+            <SheetContent
+                className="sm:max-w-[1015px] !max-h-[calc(100dvh-84px)] w-full p-0 m-0 top-[84px] flex flex-col border-0"
+                tabIndex={-1}
+                style={{ backgroundColor: "rgba(19, 35, 44, 1)" }}
+                close={false}>
+                <SheetHeader className="p-[42px] pb-0 flex-shrink-0">
+                    <div className="flex flex-col gap-2">
+                        <div className="flex justify-between items-center ">
+                            <SheetTitle className="!text-display-1">{translate("app.ui.accountHistory")}</SheetTitle>
 
-                                <button
-                                    onClick={() => onOpenChange(false)}
-                                    className="text-gray-500 hover:text-gray-700 transition-colors border-0 outline-0">
-                                    <XIcon className="h-[28px] w-[28px]" />
-                                </button>
-                            </div>
-                            <div className="text-display-2 mb-2">
-                                <span>{accountCaption}</span>
-                            </div>
-                            <TextField text={accountId} copyValue />
+                            <button
+                                onClick={() => onOpenChange(false)}
+                                className="text-gray-500 hover:text-gray-700 transition-colors border-0 outline-0">
+                                <XIcon className="h-[28px] w-[28px]" />
+                            </button>
                         </div>
-                    </SheetHeader>
-
-                    <div className="flex-1 overflow-auto" tabIndex={-1}>
-                        <SheetDescription />
-                        <AccountShow id={accountId} type="compact" />
+                        <div className="text-display-2 mb-2">
+                            <span>{accountCaption}</span>
+                        </div>
+                        <TextField text={accountId} copyValue />
                     </div>
-                </SheetContent>
-            </Sheet>
-        </>
+                </SheetHeader>
+
+                <div className="flex-1 overflow-auto" tabIndex={-1}>
+                    <SheetDescription />
+                    <AccountShow id={accountId} type="compact" />
+                </div>
+            </SheetContent>
+        </Sheet>
     );
 };
