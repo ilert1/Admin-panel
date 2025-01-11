4bcd0ef1df1c0eb33fffc6e6aa90beb680bd22ae
GrigDavid <MyEmail>
2025-01-11 21:41:54 +0400

    refactor

---
+++ source/src/components/widgets/components/LoginPageThemeSwitcher.tsx
@@ -0,0 +1,75 @@
+import SunIcon from "@/lib/icons/Sun.svg?react";
+import MoonIcon from "@/lib/icons/Moon.svg?react";
+import { Text } from "@/components/ui/text";
+import { cn } from "@/lib/utils";
+import { Translate } from "react-admin";
+import { ThemeProviderState } from "@/components/providers/ThemeProvider";
+
+interface LoginPageThemeSwitcherProps extends ThemeProviderState {
+    translate: Translate;
+}
+export const LoginPageThemeSwitcher = (props: LoginPageThemeSwitcherProps) => {
+    const { theme, setTheme, translate } = props;
+
+    const toggleTheme = () => {
+        setTheme(theme === "light" ? "dark" : "light");
+    };
+
+    return (
+        <div
+            className="flex border border-neutral-50 rounded-full items-center bg-white dark:bg-black relative w-[140px] h-[60px] overflow-hidden cursor-pointer z-[-2]"
+            onClick={toggleTheme}>
+            <div className="absolute w-full h-full flex items-center gap-0">
+                <div className="flex items-center">
+                    <div
+                        className={cn(
+                            "absolute rounded-full bg-neutral-50 overflow-hidden m-[5px] mr-0 duration-500 opacity:duration-0 z-1",
+                            theme === "dark" ? "translate-x-0 ml-[7px] opacity-100" : "translate-x-[160%] opacity-0"
+                        )}
+                        style={{
+                            boxShadow: "0px 0px 16px rgba(255, 2555, 255, 0.75)"
+                        }}>
+                        <MoonIcon
+                            style={{ backdropFilter: "blur(4px)", WebkitBackdropFilter: "blur(4px)" }}
+                            className="shadow-1 z-2"
+                        />
+                    </div>
+                    <div
+                        className={cn(
+                            "rounded-full overflow-hidden cursor-pointer ml-0 duration-500 opacity:duration-0 z-1",
+                            theme === "dark" ? "translate-x-full opacity-0" : "translate-x-[165%] opacity-100"
+                        )}>
+                        <SunIcon
+                            style={{
+                                boxShadow: "0px 0px 12px rgba(241, 197, 42, 0.75)",
+                                borderRadius: "50%",
+                                border: "0"
+                            }}
+                            className="shadow-1 z-2"
+                        />
+                    </div>
+                </div>
+                <div
+                    className={cn(
+                        "flex items-center justify-end w-full h-full z-2 ml-1",
+                        theme === "dark" ? "translate-x-0" : "-translate-x-[60%]"
+                    )}>
+                    <Text
+                        text={translate("app.login.darkTheme")}
+                        className={cn(
+                            "absolute px-[13px] py-[16px] !text-title-2 text-neutral-90 dark:text-neutral-100 duration-500 text-center w-full z-[-1]",
+                            theme === "dark" ? "opacity-100" : "opacity-0 translate-x-[0%]"
+                        )}
+                    />
+                    <Text
+                        text={translate("app.login.lightTheme")}
+                        className={cn(
+                            "absolute px-[13px] py-[16px] !text-title-2 text-neutral-90 dark:text-neutral-100 duration-500 text-center w-full z-[-1]",
+                            theme === "dark" ? "opacity-0 -translate-x-[0%]" : "opacity-100"
+                        )}
+                    />
+                </div>
+            </div>
+        </div>
+    );
+};
--- source/src/pages/LoginPage.tsx
+++ source/src/pages/LoginPage.tsx
@@ -14,10 +14,7 @@ import {
     DialogTitle
 } from "@/components/ui/dialog";
 import { LoginBackground } from "@/lib/icons/LoginBackground";
-import SunIcon from "../../public/Sun.svg?react";
-import MoonIcon from "../../public/Moon.svg?react";
-import { Text } from "@/components/ui/text";
-import { cn } from "@/lib/utils";
+import { LoginPageThemeSwitcher } from "@/components/widgets/components/LoginPageThemeSwitcher";
 
 const realm = import.meta.env.VITE_KEYCLOAK_REALM;
 const kk = import.meta.env.VITE_KEYCLOAK_URL;
@@ -107,113 +104,11 @@ export const LoginPage = () => {
             if (error) setError("");
         };
 
-    const toggleTheme = () => {
-        setTheme(theme === "light" ? "dark" : "light");
-    };
     const loginBg = LoginBackground(theme === "dark" ? "#237648" : "#62D093");
     return (
         <>
             <div className="flex flex-shrink-0 justify-end h-[84px] items-center gap-8 bg-header px-4 relative z-100 pointer-events-auto z">
-                <div
-                    className="flex border border-neutral-50 rounded-full items-center bg-white dark:bg-black relative w-[140px] h-[60px] overflow-hidden cursor-pointer z-[-2]"
-                    onClick={toggleTheme}>
-                    <div className="absolute w-full h-full flex items-center gap-0">
-                        <div className="flex items-center">
-                            <div
-                                className={cn(
-                                    "absolute rounded-full bg-neutral-50 overflow-hidden m-[5px] mr-0 duration-500 opacity:duration-0 z-1",
-                                    theme === "dark"
-                                        ? "translate-x-0 ml-[7px] opacity-100"
-                                        : "translate-x-[160%] opacity-0"
-                                )}
-                                style={{
-                                    boxShadow: "0px 0px 16px rgba(255, 2555, 255, 0.75)"
-                                }}>
-                                <MoonIcon
-                                    style={{ backdropFilter: "blur(4px)", WebkitBackdropFilter: "blur(4px)" }}
-                                    className="shadow-1 z-2"
-                                />
-                            </div>
-                            <div
-                                className={cn(
-                                    "rounded-full overflow-hidden cursor-pointer ml-0 duration-500 opacity:duration-0 z-1",
-                                    theme === "dark" ? "translate-x-full opacity-0" : "translate-x-[165%] opacity-100"
-                                )}>
-                                <SunIcon
-                                    style={{
-                                        boxShadow: "0px 0px 12px rgba(241, 197, 42, 0.75)",
-                                        borderRadius: "50%",
-                                        border: "0"
-                                    }}
-                                    className="shadow-1 z-2"
-                                />
-                            </div>
-                        </div>
-                        <div
-                            className={cn(
-                                "flex items-center justify-end w-full h-full z-2 ml-1",
-                                theme === "dark" ? "translate-x-0" : "-translate-x-[60%]"
-                            )}>
-                            <Text
-                                text={translate("app.login.darkTheme")}
-                                className={cn(
-                                    "absolute px-[13px] py-[16px] !text-title-2 text-neutral-90 dark:text-neutral-100 duration-500 text-center w-full z-[-1]",
-                                    theme === "dark" ? "opacity-100" : "opacity-0 translate-x-[0%]"
-                                )}
-                            />
-                            <Text
-                                text={translate("app.login.lightTheme")}
-                                className={cn(
-                                    "absolute px-[13px] py-[16px] !text-title-2 text-neutral-90 dark:text-neutral-100 duration-500 text-center w-full z-[-1]",
-                                    theme === "dark" ? "opacity-0 -translate-x-[0%]" : "opacity-100"
-                                )}
-                            />
-                        </div>
-                    </div>
-                </div>
-                {/* <div
-                    className="flex relative w-32 h-14 rounded-full bg-white dark:bg-black border border-neutral-200 cursor-pointer overflow-hidden"
-                    onClick={toggleTheme}>
-                    <div className="absolute inset-0 flex items-center">
-                        <div
-                            className={`
-          absolute p-2 rounded-full bg-neutral-100
-          transition-all duration-500 ease-in-out
-          ${theme === "dark" ? "translate-x-2 opacity-100" : "translate-x-full opacity-0"}
-        `}>
-                            <MoonIcon className="w-6 h-6 text-neutral-800" />
-                        </div>
-
-                        <div
-                            className={`
-          absolute p-2 rounded-full
-          transition-all duration-500 ease-in-out
-          ${theme === "dark" ? "-translate-x-full opacity-0" : "translate-x-2 opacity-100"}
-        `}>
-                            <SunIcon className="w-6 h-6 text-yellow-500" />
-                        </div>
-                    </div>
-
-                    <div className="absolute inset-0 flex items-center justify-end pr-4">
-                        <span
-                            className={`
-          absolute text-sm font-medium
-          transition-all duration-500 ease-in-out
-          ${theme === "dark" ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"}
-        `}>
-                            Темная
-                        </span>
-
-                        <span
-                            className={`
-          absolute text-sm font-medium
-          transition-all duration-500 ease-in-out
-          ${theme === "dark" ? "translate-x-full opacity-0" : "translate-x-0 opacity-100"}
-        `}>
-                            Светлая
-                        </span>
-                    </div>
-                </div> */}
+                <LoginPageThemeSwitcher theme={theme} setTheme={setTheme} translate={translate} />
                 <LangSwitcher />
             </div>
 
