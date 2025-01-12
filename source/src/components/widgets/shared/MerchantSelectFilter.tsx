097d01a5cda4ca7a2c505803884a8712907c50e0
zulus-ru <maxsaxmax@mail.ru>
2025-01-12 13:42:36 +0300

    little fix

--- source/src/components/widgets/shared/MerchantSelectFilter.tsx
+++ source/src/components/widgets/shared/MerchantSelectFilter.tsx
@@ -73,7 +73,6 @@ export const MerchantSelectFilter = ({ merchant, onMerchantChanged, resource }: 
                     />
                 </Button>
             </PopoverTrigger>
-            ``
             <PopoverContent className="md:w-[--radix-popover-trigger-width] md:max-h-[--radix-popover-content-available-height] p-0 z-[60] min-w-[8rem] overflow-hidden rounded-4 border border-green-50 bg-white dark:bg-red-0 shadow-1 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 ">
                 <Command filter={merchantFilter}>
                     <CommandInput placeholder={translate("app.ui.actions.search")} />
