75d03cbf46930827b478affe2eb306e099b540c5
David <email@mail.com>
2025-02-05 09:59:36 +0400

    feat: Select experiemental changes

--- source/src/components/ui/select.tsx
+++ source/src/components/ui/select.tsx
@@ -58,7 +58,7 @@ const SelectTrigger = React.forwardRef<React.ElementRef<typeof SelectPrimitive.T
                         </>
                     </SelectPrimitive.Icon>
                 </SelectPrimitive.Trigger>
-                {isError && <span className="!text-note-1 inline !block sm:!hidden !mt-[4px]">{errorMessage}</span>}
+                {isError && <span className="!text-note-1 block sm:!hidden !mt-[4px]">{errorMessage}</span>}
             </>
         );
     }
@@ -69,32 +69,57 @@ const SelectContent = React.forwardRef<
     React.ElementRef<typeof SelectPrimitive.Content>,
     React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
 >(({ className, children, position = "popper", ...props }, ref) => {
-    const { ref: selectRef, isOut } = useIsOutOfView();
+    const [maxHeight, setMaxHeight] = React.useState<string>("400");
+    const selectRef = React.useRef<HTMLDivElement>(null);
+
+    React.useEffect(() => {
+        console.log("availableSpace");
+        const calculateMaxHeight = () => {
+            if (selectRef.current) {
+                const viewportHeight = window.innerHeight;
+                const selectRect = selectRef.current.getBoundingClientRect();
+                const topSpace = selectRect.top;
+                const bottomSpace = viewportHeight - selectRect.bottom;
+
+                const availableSpace = Math.max(topSpace, bottomSpace) - 20;
+                console.log(availableSpace);
+                setMaxHeight(Math.min(availableSpace, 400).toString());
+            }
+        };
+
+        calculateMaxHeight();
+        window.addEventListener("resize", calculateMaxHeight);
+
+        return () => window.removeEventListener("resize", calculateMaxHeight);
+    }, [selectRef.current?.clientHeight]);
 
     return (
         <SelectPrimitive.Portal>
-            <>
-                <SelectPrimitive.Content
-                    ref={selectRef}
-                    className={cn(
-                        "relative z-[60] max-h-96 min-w-[8rem] overflow-hidden rounded-4 border border-green-50 bg-white dark:bg-neutral-100 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-                        position === "popper" && "data-[side=bottom]:translate-y-1 data-[side=top]:-translate-y-1",
-                        isOut ? "" : "max-h-10",
-                        className
-                    )}
-                    position={position}
-                    {...props}>
-                    <ScrollArea className="h-full overflow-auto">
-                        <SelectPrimitive.Viewport
-                            className={cn(
-                                "p-0",
-                                position === "popper" && "w-full min-w-[var(--radix-select-trigger-width)]"
-                            )}>
-                            {children}
-                        </SelectPrimitive.Viewport>
-                    </ScrollArea>
-                </SelectPrimitive.Content>
-            </>
+            <SelectPrimitive.Content
+                ref={selectRef}
+                className={cn(
+                    "relative z-[60] min-w-[8rem] overflow-hidden rounded-4 border border-green-50 bg-white dark:bg-neutral-100",
+                    "data-[state=open]:animate-in data-[state=closed]:animate-out",
+                    "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+                    "data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
+                    "data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2",
+                    "data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 ",
+                    position === "popper" && "data-[side=bottom]:translate-y-1 data-[side=top]:-translate-y-1",
+                    className
+                )}
+                position={position}
+                style={{ maxHeight: `${maxHeight}px` }}
+                {...props}>
+                <ScrollArea className="h-full overflow-auto">
+                    <SelectPrimitive.Viewport
+                        className={cn(
+                            "p-0",
+                            position === "popper" && "w-full min-w-[var(--radix-select-trigger-width)]"
+                        )}>
+                        {children}
+                    </SelectPrimitive.Viewport>
+                </ScrollArea>
+            </SelectPrimitive.Content>
         </SelectPrimitive.Portal>
     );
 });
