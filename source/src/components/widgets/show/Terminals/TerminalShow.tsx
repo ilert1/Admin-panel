b1e428629f43defd7be77bdab6d0ba37c77f8fc4
David <email@mail.com>
2025-02-13 11:39:00 +0400

    feat: TerminalShow page done

--- source/src/components/widgets/show/Terminals/TerminalShow.tsx
+++ source/src/components/widgets/show/Terminals/TerminalShow.tsx
@@ -5,7 +5,6 @@ import { FeesResource } from "@/data";
 import { TextField } from "@/components/ui/text-field";
 import { useQuery } from "react-query";
 import { LoadingBlock } from "@/components/ui/loading";
-import { Text } from "@/components/ui/text";
 import { Label } from "@/components/ui/label";
 
 interface TerminalShowProps {
@@ -22,6 +21,8 @@ export const TerminalShow = (props: TerminalShowProps) => {
         queryKey: ["terminal-fees", provider, id],
         queryFn: async () => {
             const { data } = await dataProvider.getOne(`provider/${provider}/terminal`, { id });
+            console.log(data);
+
             setData(data);
         }
     });
@@ -31,19 +32,38 @@ export const TerminalShow = (props: TerminalShowProps) => {
     }
 
     return (
-        <div className="px-[45px] ">
-            <div className="flex flex-col gap-2">
-                <TextField text={id} copyValue className="text-display-4" />
-                <div className="flex gap-6">
-                    <TextField text={data.verbose_name} label={translate("resources.terminals.fields.verbose_name")} />
-                    <TextField text={data.provider} label={translate("resources.terminals.fields.description")} />
-                    <TextField text={data.description ?? ""} label={translate("resources.terminals.fields.provider")} />
+        <div className="px-[45px] flex flex-col gap-4">
+            <div className="flex flex-col gap-6">
+                <div className="flex flex-col gap-2">
+                    <TextField text={id} copyValue className="text-display-4" />
+                    <div className="flex gap-6">
+                        <TextField
+                            text={data.verbose_name}
+                            label={translate("resources.terminals.fields.verbose_name")}
+                        />
+                        <TextField text={data.provider} label={translate("resources.terminals.fields.description")} />
+                        <TextField
+                            text={data.description ?? ""}
+                            label={translate("resources.terminals.fields.provider")}
+                        />
+                    </div>
+                    <div className="">
+                        <Label className="mb-0">{translate("resources.terminals.fields.auth")}</Label>
+                        <TextField text={JSON.stringify(data.auth)} copyValue />
+                    </div>
                 </div>
-                <div className="">
-                    <Label className="mb-0">{translate("resources.terminals.fields.auth")}</Label>
-                    <TextField text={JSON.stringify(data.auth)} />
-                    {/* <span className="text-ellipsis overflow-hidden text-nowrap block">{JSON.stringify(data.auth)}</span> */}
-                </div>
+                {/* TODO */}
+                {/* <div className="flex flex-col gap-3">
+                    <TextField text="Terminal account" className="text-display-3" />
+                    <div className="flex gap-6">
+                        <TextField text="" label="Type" />
+                        <TextField text="" label="Account balance" />
+                    </div>
+                    <div className="flex justify-between">
+                        <div className="flex gap-2"></div>
+                        <Button></Button>
+                    </div>
+                </div> */}
             </div>
             <Fees
                 fees={data?.fees}
