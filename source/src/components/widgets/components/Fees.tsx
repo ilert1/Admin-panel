fbf642e693132362340adcdf2f72ef79237c52f5
David <email@mail.com>
2025-03-03 14:51:16 +0400

    fix: Fixed size for dialogs

--- source/src/components/widgets/components/Fees.tsx
+++ source/src/components/widgets/components/Fees.tsx
@@ -113,9 +113,7 @@ export const Fees = memo((props: FeesProps) => {
             </div>
             {addFee && (
                 <div className="flex justify-end">
-                    <Button
-                        onClick={() => setAddNewOpen(true)}
-                        className="w-full my-6 sm:w-2/5 lg:w-1/4 flex gap-[4px]">
+                    <Button onClick={() => setAddNewOpen(true)} className="w-full my-6 sm:w-2/5 flex gap-[4px]">
                         <CircleChevronRight className="w-[16px] h-[16px]" />
                         {translate("resources.direction.fees.addFee")}
                     </Button>
