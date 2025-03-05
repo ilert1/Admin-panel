3233b19f4c4c6fdf52510bc46fe07f35359a29cf
checkkick <tuskancik226@gmail.com>
2025-03-05 12:32:10 +0300

    feat: not cleat provider in terminalList after reload page

--- source/src/components/widgets/lists/Terminals/TerminalsListFilter.tsx
+++ source/src/components/widgets/lists/Terminals/TerminalsListFilter.tsx
@@ -1,4 +1,4 @@
-import { UIEvent, useMemo, useState } from "react";
+import { UIEvent, useEffect, useMemo, useState } from "react";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useInfiniteGetList, useTranslate } from "react-admin";
 import { LoadingBalance } from "@/components/ui/loading";
@@ -11,6 +11,7 @@ export const TerminalsListFilter = ({ selectProvider = () => {} }: { selectProvi
         isFetchingNextPage,
         hasNextPage,
         isFetching,
+        isFetched,
         fetchNextPage: providersNextPage
     } = useInfiniteGetList<ProviderWithId>("provider", {
         pagination: { perPage: 25, page: 1 },
@@ -19,13 +20,24 @@ export const TerminalsListFilter = ({ selectProvider = () => {} }: { selectProvi
 
     const translate = useTranslate();
 
-    const [providerName, setProviderName] = useState("");
+    const [providerName, setProviderName] = useState(localStorage.getItem("providerInTerminals") || "");
     const providersLoadingProcess = useMemo(() => isFetchingNextPage && hasNextPage, [isFetchingNextPage, hasNextPage]);
 
     const onProviderChanged = (provider: string) => {
+        localStorage.setItem("providerInTerminals", provider);
         setProviderName(provider);
         selectProvider(provider);
     };
+
+    useEffect(() => {
+        if (
+            isFetched &&
+            providersData?.pages.find(providerItem => providerItem.data.find(item => item.name === providerName))
+        ) {
+            selectProvider(providerName);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [providersLoadingProcess, providersData?.pages]);
 
     const providerScrollHandler = async (e: UIEvent<HTMLDivElement>) => {
         const target = e.target as HTMLElement;
