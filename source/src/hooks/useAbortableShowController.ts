2b67b56e648fe9af4aa25a83a77dcd338bda0498
checkkick <tuskancik226@gmail.com>
2025-03-20 14:34:49 +0300

    feat: add custom hooks for abortable controllers

---
+++ source/src/hooks/useAbortableShowController.ts
@@ -0,0 +1,26 @@
+import { useEffect, useMemo, useRef } from "react";
+import { useShowController, ShowControllerProps, ShowControllerResult, RaRecord } from "react-admin";
+
+export const useAbortableShowController = <RecordType extends RaRecord = any>(
+    props: ShowControllerProps<RecordType>
+): ShowControllerResult<RecordType> => {
+    const abortControllerRef = useRef(new AbortController());
+
+    useEffect(() => {
+        const abortController = abortControllerRef.current;
+        return () => abortController.abort();
+    }, []);
+
+    const showController = useShowController<RecordType>({
+        ...props,
+        queryOptions: {
+            ...props.queryOptions,
+            meta: {
+                ...(props.queryOptions?.meta || {}),
+                signal: abortControllerRef.current.signal
+            }
+        }
+    });
+
+    return useMemo(() => showController, [showController]);
+};
