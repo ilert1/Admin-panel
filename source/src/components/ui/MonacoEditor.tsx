d1a8572ea76a1db1d6dde3762c6a63051891bbb4
checkkick <tuskancik226@gmail.com>
2025-04-11 15:45:15 +0300

    fix: change any props for not required if not editable mode

--- source/src/components/ui/MonacoEditor.tsx
+++ source/src/components/ui/MonacoEditor.tsx
@@ -8,29 +8,38 @@ interface MonacoEditorProps {
     width?: string;
     code: string;
     disabled?: boolean;
-    setCode: (value: string) => void;
-    onErrorsChange: (hasErrors: boolean) => void;
-    onValidChange: (isValid: boolean) => void;
+    setCode?: (value: string) => void;
+    onErrorsChange?: (hasErrors: boolean) => void;
+    onValidChange?: (isValid: boolean) => void;
     onMountEditor?: OnMount;
 }
 
 export const MonacoEditor = (props: MonacoEditorProps) => {
-    const { code, disabled = false, setCode, onErrorsChange, onValidChange, onMountEditor = () => {} } = props;
+    const {
+        code,
+        disabled = false,
+        setCode = () => {},
+        onErrorsChange,
+        onValidChange,
+        onMountEditor = () => {}
+    } = props;
     const { theme } = useTheme();
 
     const isDark = theme === "dark";
 
     const validateCode = useCallback(
         (value: string) => {
-            try {
-                const parsed = JSON.parse(value || "{}");
-                if (value.trim() === "" || Object.keys(parsed).length === 0) {
+            if (onValidChange) {
+                try {
+                    const parsed = JSON.parse(value || "{}");
+                    if (value.trim() === "" || Object.keys(parsed).length === 0) {
+                        onValidChange(false);
+                    } else {
+                        onValidChange(true);
+                    }
+                } catch (error) {
                     onValidChange(false);
-                } else {
-                    onValidChange(true);
                 }
-            } catch (error) {
-                onValidChange(false);
             }
         },
         [onValidChange]
@@ -48,7 +57,9 @@ export const MonacoEditor = (props: MonacoEditorProps) => {
 
     const handleValidation = useCallback<OnValidate>(
         markers => {
-            onErrorsChange(markers.length > 0);
+            if (onErrorsChange) {
+                onErrorsChange(markers.length > 0);
+            }
         },
         [onErrorsChange]
     );
