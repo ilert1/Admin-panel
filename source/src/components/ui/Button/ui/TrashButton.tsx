f21b9d8fdcce15951f8e4c84fe27032c0063e572
checkkick <tuskancik226@gmail.com>
2025-04-15 16:39:45 +0300

    feat: TrashButton add prop for disabled

--- source/src/components/ui/Button/ui/TrashButton.tsx
+++ source/src/components/ui/Button/ui/TrashButton.tsx
@@ -1,10 +1,14 @@
 import { Trash2 } from "lucide-react";
 import { Button } from "../button";
 
-export const TrashButton = ({ onClick }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+export const TrashButton = ({ onClick, disabled }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
     return (
         <div className="flex justify-center">
-            <Button onClick={onClick} variant="text_btn" className="h-8 w-8 bg-transparent p-0">
+            <Button
+                disabled={disabled}
+                onClick={onClick}
+                variant="text_btn"
+                className="h-8 w-8 bg-transparent p-0 [&:disabled>svg]:text-neutral-70">
                 <Trash2 className="h-6 w-6 text-red-40 hover:text-red-30 active:text-red-50" />
             </Button>
         </div>
