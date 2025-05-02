75f0374b98f453b42c5a25cc16d5b2d4ea559d24
Konstantin <tuskancik226@gmail.com>
2025-05-02 19:36:56 +0300

    fix: input component background transparent

--- source/src/components/ui/Input/input.tsx
+++ source/src/components/ui/Input/input.tsx
@@ -191,7 +191,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
                         disabled={disabled}
                         spellCheck="false"
                         className={cn(
-                            "z-1 flex h-9 w-full rounded-4 bg-inherit px-3 py-2 text-sm text-inherit ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus:outline-none",
+                            "z-1 flex h-9 w-full rounded-4 bg-transparent px-3 py-2 text-sm text-inherit ring-offset-background file:border-0 file:text-sm file:font-medium focus:outline-none",
                             type === "password_masked" && !showPassword && "input-masked",
                             className
                         )}
