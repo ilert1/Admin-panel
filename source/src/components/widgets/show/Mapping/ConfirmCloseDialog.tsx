685fc5f44ea22f7ec3e3a9efcaa91b2d9ca1946b
David <email>
2025-06-06 13:57:21 +0400

    fix: removed dep

--- source/src/components/widgets/show/Mapping/ConfirmCloseDialog.tsx
+++ source/src/components/widgets/show/Mapping/ConfirmCloseDialog.tsx
@@ -7,7 +7,6 @@ import {
     DialogHeader,
     DialogTitle
 } from "@/components/ui/dialog";
-import { TextField } from "@/components/ui/text-field";
 import { useTranslate } from "react-admin";
 
 export interface ConfirmCloseDialogProps {
