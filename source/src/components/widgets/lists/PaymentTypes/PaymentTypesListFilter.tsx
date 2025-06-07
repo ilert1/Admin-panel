d5787d33c32de6e930322f2c6c9efc9971f2c919
David <email>
2025-06-07 12:40:39 +0400

    feat: Payment type filters done

---
+++ source/src/components/widgets/lists/PaymentTypes/PaymentTypesListFilter.tsx
@@ -0,0 +1,110 @@
+import { useState } from "react";
+import { FilterButtonGroup } from "../../components/FilterButtonGroup";
+import { AnimatedContainer } from "../../components/AnimatedContainer";
+import { ResourceHeaderTitle } from "../../components/ResourceHeaderTitle";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/Input/input";
+import usePaymentTypesListFilter from "./usePaymentTypesListFilter";
+import { Button } from "@/components/ui/Button";
+import { CirclePlus } from "lucide-react";
+import { PaymentCategory } from "@/api/enigma/blowFishEnigmaAPIService.schemas";
+
+interface PaymentTypesListFilterProps {
+    handleCreateClicked: () => void;
+}
+
+export const PaymentTypesListFilter = (props: PaymentTypesListFilterProps) => {
+    const { handleCreateClicked } = props;
+
+    const { translate, code, title, category, onCategoryChanged, onClearFilters, onCodeChanged, onTitleChanged } =
+        usePaymentTypesListFilter();
+
+    const [openFiltersClicked, setOpenFiltersClicked] = useState(false);
+    const paymentTypeCategories = Object.keys(PaymentCategory);
+    const clearDisabled = !code && !title && !category;
+
+    return (
+        <div className="mb-4">
+            <div className="mb-6 flex flex-wrap justify-between gap-2">
+                <ResourceHeaderTitle />
+                <div className="flex flex-col gap-2 sm:flex-row">
+                    <div className="flex flex-col gap-4 sm:flex-row">
+                        <FilterButtonGroup
+                            filterList={[code, title, category]}
+                            onClearFilters={onClearFilters}
+                            open={openFiltersClicked}
+                            onOpenChange={setOpenFiltersClicked}
+                            clearButtonDisabled={clearDisabled}
+                        />
+                    </div>
+                    <div className="flex justify-end">
+                        <Button onClick={handleCreateClicked} variant="default" className="flex gap-[4px]">
+                            <CirclePlus className="h-[16px] w-[16px]" />
+
+                            <span className="text-title-1">
+                                {translate("resources.paymentTools.paymentType.createNew")}
+                            </span>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+            <AnimatedContainer open={openFiltersClicked}>
+                <div className="flex flex-col gap-2">
+                    <div className="flex flex-wrap gap-2 sm:flex-nowrap">
+                        <Input
+                            label={translate("resources.paymentTools.paymentType.fields.code")}
+                            labelSize="title-2"
+                            value={code}
+                            onChange={onCodeChanged}
+                            className="min-w-40"
+                        />
+                        <Input
+                            label={translate("resources.paymentTools.paymentType.fields.title")}
+                            labelSize="title-2"
+                            value={title}
+                            onChange={onTitleChanged}
+                            className="min-w-40"
+                        />
+                        <div className="flex w-full flex-wrap gap-2 sm:flex-nowrap">
+                            <div className="flex min-w-[50%] max-w-full flex-1 flex-col gap-1 sm:min-w-44 sm:max-w-[25%]">
+                                <Label variant="title-2" className="mb-0">
+                                    {translate("resources.paymentTools.paymentType.fields.category")}
+                                </Label>
+
+                                <Select
+                                    value={category}
+                                    onValueChange={value =>
+                                        value === "null" ? onCategoryChanged("") : onCategoryChanged(value)
+                                    }>
+                                    <SelectTrigger className="h-[38px] !w-full text-ellipsis">
+                                        <SelectValue
+                                            placeholder={translate(
+                                                "resources.transactions.filter.filterAllPlaceholder"
+                                            )}
+                                        />
+                                    </SelectTrigger>
+
+                                    <SelectContent>
+                                        <SelectGroup>
+                                            <SelectItem value="null">
+                                                {translate("resources.transactions.filter.showAll")}
+                                            </SelectItem>
+                                            {paymentTypeCategories.map(el => {
+                                                return (
+                                                    <SelectItem key={el} value={el}>
+                                                        {el}
+                                                    </SelectItem>
+                                                );
+                                            })}
+                                        </SelectGroup>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </AnimatedContainer>
+        </div>
+    );
+};
---
+++ source/src/components/widgets/lists/PaymentTypes/usePaymentTypesListFilter.tsx
@@ -0,0 +1,61 @@
+import { CallbackStatusEnum } from "@/api/callbridge/blowFishCallBridgeAPIService.schemas";
+import { debounce } from "lodash";
+import { ChangeEvent, useState } from "react";
+import { useListContext, useTranslate } from "react-admin";
+
+const usePaymentTypesListFilter = () => {
+    const translate = useTranslate();
+
+    const { filterValues, setFilters, displayedFilters, setPage } = useListContext();
+
+    const [code, setCode] = useState(filterValues?.code || "");
+    const [title, setTitle] = useState(filterValues?.title || "");
+    const [category, setCategory] = useState(filterValues?.category || "");
+
+    const onPropertySelected = debounce((value: string, type: "code" | "title" | "category") => {
+        if (value) {
+            setFilters({ ...filterValues, [type]: value }, displayedFilters, true);
+        } else {
+            Reflect.deleteProperty(filterValues, type);
+            setFilters(filterValues, displayedFilters, true);
+        }
+        setPage(1);
+    }, 300);
+
+    const onCodeChanged = (e: ChangeEvent<HTMLInputElement>) => {
+        setCode(e.target.value);
+        onPropertySelected(e.target.value, "code");
+    };
+
+    const onTitleChanged = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value);
+        onPropertySelected(e.target.value, "title");
+    };
+
+    const onCategoryChanged = (value: string) => {
+        setCategory(value);
+        onPropertySelected(value, "category");
+    };
+
+    const onClearFilters = () => {
+        setFilters({}, displayedFilters, true);
+        setPage(1);
+        setCode("");
+        setTitle("");
+        setCategory("");
+    };
+    // code, title, category
+
+    return {
+        translate,
+        code,
+        title,
+        category,
+        onCategoryChanged,
+        onClearFilters,
+        onCodeChanged,
+        onTitleChanged
+    };
+};
+
+export default usePaymentTypesListFilter;
