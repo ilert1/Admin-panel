a52216a1db1c13f1f82789618efdab833f2516bd
Максим Сулин <zulus@MacBook-Pro-Maksim.local>
2025-06-09 17:50:09 +0300

    little translations edit SPI TPI

--- source/src/components/widgets/lists/TerminalPaymentInstruments/CreateTerminalPaymentInstrumentsDialog.tsx
+++ source/src/components/widgets/lists/TerminalPaymentInstruments/CreateTerminalPaymentInstrumentsDialog.tsx
@@ -26,7 +26,7 @@ export const CreateTerminalPaymentInstrumentsDialog = ({
                 className="max-w-full !overflow-y-auto bg-muted sm:max-h-[100dvh] sm:w-[716px]">
                 <DialogHeader>
                     <DialogTitle className="mb-4 text-center">
-                        {translate("resources.paymentTools.terminalPaymentInstruments.createTerminalPaymentInstrument")}
+                        {translate("resources.paymentTools.terminalPaymentInstruments.creatingTerminalPaymentInstrument")}
                     </DialogTitle>
                     <DialogDescription />
 
