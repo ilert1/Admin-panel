8608cb9659d8c3789be263296ecf03991a2c76b5
checkkick <tuskancik226@gmail.com>
2025-06-17 16:55:24 +0300

    fix: TPI show non scrollable fixed

--- source/src/components/widgets/lists/TerminalPaymentInstruments/ShowTerminalPaymentInstrumentsSheet.tsx
+++ source/src/components/widgets/lists/TerminalPaymentInstruments/ShowTerminalPaymentInstrumentsSheet.tsx
@@ -16,31 +16,31 @@ export const ShowTerminalPaymentInstrumentsSheet = ({
     const translate = useTranslate();
 
     return (
-        <>
-            <Sheet open={open} onOpenChange={onOpenChange}>
-                <SheetContent
-                    className="top-[84px] m-0 flex h-full !max-h-[calc(100dvh-84px)] w-full flex-col overflow-hidden border-0 p-0 sm:max-w-[1015px]"
-                    tabIndex={-1}
-                    close={false}>
-                    <div className="flex-shrink-0 p-4 pb-0 md:p-[42px] md:pb-0">
-                        <div className="flex items-center justify-between">
-                            <SheetTitle className="!text-display-1">
-                                {translate("resources.paymentTools.terminalPaymentInstruments.show")}
-                            </SheetTitle>
+        <Sheet open={open} onOpenChange={onOpenChange}>
+            <SheetContent
+                className="top-[84px] m-0 flex !max-h-[calc(100dvh-84px)] w-full flex-col border-0 p-0 sm:max-w-[1015px]"
+                tabIndex={-1}
+                close={false}>
+                <div className="flex-shrink-0 p-4 pb-0 md:p-[42px] md:pb-0">
+                    <div className="flex items-center justify-between">
+                        <SheetTitle className="!text-display-1">
+                            {translate("resources.paymentTools.terminalPaymentInstruments.show")}
+                        </SheetTitle>
 
-                            <button
-                                onClick={() => onOpenChange(false)}
-                                className="border-0 text-gray-500 outline-0 transition-colors hover:text-gray-700">
-                                <XIcon className="h-[28px] w-[28px]" />
-                            </button>
-                        </div>
+                        <button
+                            onClick={() => onOpenChange(false)}
+                            className="border-0 text-gray-500 outline-0 transition-colors hover:text-gray-700">
+                            <XIcon className="h-[28px] w-[28px]" />
+                        </button>
                     </div>
+                </div>
 
+                <div className="flex-1 overflow-auto" tabIndex={-1}>
                     <TerminalPaymentInstrumentsShow id={id} onOpenChange={onOpenChange} />
+                </div>
 
-                    <SheetDescription />
-                </SheetContent>
-            </Sheet>
-        </>
+                <SheetDescription />
+            </SheetContent>
+        </Sheet>
     );
 };
