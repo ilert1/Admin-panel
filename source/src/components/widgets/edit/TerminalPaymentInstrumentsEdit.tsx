afced73db0e0325bcb38a32b9765ca2b87fe4fda
checkkick <tuskancik226@gmail.com>
2025-06-25 10:47:45 +0300

    feat: TPI edit disabled fields provider, terminal, SPI

--- source/src/components/widgets/edit/TerminalPaymentInstrumentsEdit.tsx
+++ source/src/components/widgets/edit/TerminalPaymentInstrumentsEdit.tsx
@@ -1,7 +1,7 @@
 import { useRefresh, useTranslate } from "react-admin";
 import { useForm } from "react-hook-form";
 import { Input, InputTypes } from "@/components/ui/Input/input";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/Button";
 import { Loading } from "@/components/ui/loading";
 import { z } from "zod";
@@ -13,9 +13,6 @@ import { Label } from "@/components/ui/label";
 import { useAppToast } from "@/components/ui/toast/useAppToast";
 import { useQuery } from "@tanstack/react-query";
 import { TerminalPaymentInstrumentsProvider } from "@/data/terminalPaymentInstruments";
-import { SystemPaymentInstrumentsProvider } from "@/data/systemPaymentInstruments";
-import { ProvidersDataProvider, TerminalsDataProvider } from "@/data";
-import { PopoverSelect } from "../components/Selects/PopoverSelect";
 import { DirectionType, TerminalPaymentInstrumentStatus } from "@/api/enigma/blowFishEnigmaAPIService.schemas";
 import {
     Select,
@@ -26,7 +23,6 @@ import {
     SelectType,
     SelectValue
 } from "@/components/ui/select";
-import { ProviderSelect } from "../components/Selects/ProviderSelect";
 
 export interface TerminalPaymentInstrumentsEditProps {
     id: string;
@@ -35,12 +31,7 @@ export interface TerminalPaymentInstrumentsEditProps {
 
 export const TerminalPaymentInstrumentsEdit = ({ id, onClose = () => {} }: TerminalPaymentInstrumentsEditProps) => {
     const terminalPaymentInstrumentsProvider = new TerminalPaymentInstrumentsProvider();
-    const systemPaymentInstrumentsProvider = new SystemPaymentInstrumentsProvider();
-    const providersDataProvider = new ProvidersDataProvider();
-    const terminalsDataProvider = new TerminalsDataProvider();
     const statuses = Object.keys(TerminalPaymentInstrumentStatus);
-
-    const [providerName, setProviderName] = useState("");
 
     const {
         data: terminalPaymentInstrumentsData,
@@ -53,48 +44,10 @@ export const TerminalPaymentInstrumentsEdit = ({ id, onClose = () => {} }: Termi
         select: data => data.data
     });
 
-    const { isLoading: systemPaymentInstrumentsDataLoading, data: systemPaymentInstrumentsData } = useQuery({
-        queryKey: ["systemPaymentInstruments"],
-        queryFn: async () => await systemPaymentInstrumentsProvider.getListWithoutPagination()
-    });
-
-    const {
-        data: providersData,
-        isLoading: isProvidersLoading,
-        isFetching: isProvidersFetching
-    } = useQuery({
-        queryKey: ["providers", "filter"],
-        queryFn: () => providersDataProvider.getListWithoutPagination(),
-        select: data => data.data
-    });
-
-    const {
-        data: terminalsData,
-        isLoading: isTerminalsLoading,
-        isFetching: isTerminalsFetching
-    } = useQuery({
-        queryKey: ["terminals", "filter", providerName],
-        queryFn: () => terminalsDataProvider.getListWithoutPagination(["provider"], [providerName]),
-        enabled: !!providerName,
-        select: data => data.data
-    });
-
-    const providersLoadingProcess = useMemo(
-        () => isProvidersLoading || isProvidersFetching,
-        [isProvidersFetching, isProvidersLoading]
-    );
-
-    const terminalsLoadingProcess = useMemo(
-        () => isTerminalsLoading || isTerminalsFetching,
-        [isTerminalsFetching, isTerminalsLoading]
-    );
-
     const refresh = useRefresh();
     const translate = useTranslate();
     const appToast = useAppToast();
 
-    const [systemPaymentInstrumentCode, setSystemPaymentInstrumentCode] = useState("");
-    const [terminalValueName, setTerminalValueName] = useState("");
     const [monacoEditorMounted, setMonacoEditorMounted] = useState(false);
     const [hasErrors, setHasErrors] = useState(false);
     const [hasValid, setHasValid] = useState(true);
@@ -146,21 +99,11 @@ export const TerminalPaymentInstrumentsEdit = ({ id, onClose = () => {} }: Termi
                     JSON.stringify(terminalPaymentInstrumentsData.terminal_specific_parameters, null, 2) || "{}"
             };
 
-            setProviderName(terminalPaymentInstrumentsData.terminal.provider);
-            setSystemPaymentInstrumentCode(terminalPaymentInstrumentsData.system_payment_instrument_code);
-            setTerminalValueName(terminalPaymentInstrumentsData.terminal.verbose_name);
-
             form.reset(updatedValues);
             setIsFinished(true);
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [terminalPaymentInstrumentsData, isLoadingTerminalPaymentInstrumentsData, isFetchedAfterMount]);
-
-    const onChangeProviderName = (val: string) => {
-        setProviderName(val);
-        setTerminalValueName("");
-        form.setValue("terminal_id", "");
-    };
 
     const onSubmit = async (data: z.infer<typeof formSchema>) => {
         if (submitButtonDisabled) return;
@@ -214,82 +157,35 @@ export const TerminalPaymentInstrumentsEdit = ({ id, onClose = () => {} }: Termi
                 <div className="flex flex-col flex-wrap">
                     <div className="grid grid-cols-1 sm:grid-cols-2">
                         <div className="w-full p-2">
-                            <Label>{translate("resources.terminals.selectHeader")}</Label>
-
-                            <ProviderSelect
-                                style="Grey"
-                                providers={providersData || []}
-                                value={providerName}
-                                onChange={onChangeProviderName}
-                                disabled={providersLoadingProcess}
-                                modal
+                            <Input
+                                value={terminalPaymentInstrumentsData.terminal.provider}
+                                variant={InputTypes.GRAY}
+                                label={translate("resources.terminals.selectHeader")}
+                                disabled
                             />
                         </div>
 
-                        <FormField
-                            control={form.control}
-                            name="terminal_id"
-                            render={({ field, fieldState }) => (
-                                <FormItem className="w-full p-2">
-                                    <Label>
-                                        {translate(
-                                            "resources.paymentSettings.terminalPaymentInstruments.fields.terminal_id"
-                                        )}
-                                    </Label>
+                        <div className="w-full p-2">
+                            <Input
+                                value={terminalPaymentInstrumentsData.terminal.verbose_name}
+                                variant={InputTypes.GRAY}
+                                label={translate(
+                                    "resources.paymentSettings.terminalPaymentInstruments.fields.terminal_id"
+                                )}
+                                disabled
+                            />
+                        </div>
 
-                                    <PopoverSelect
-                                        variants={terminalsData || []}
-                                        value={terminalValueName}
-                                        idField="terminal_id"
-                                        setIdValue={field.onChange}
-                                        onChange={setTerminalValueName}
-                                        variantKey="verbose_name"
-                                        placeholder={
-                                            providerName
-                                                ? translate("resources.terminals.selectPlaceholder")
-                                                : translate("resources.direction.noTerminals")
-                                        }
-                                        commandPlaceholder={translate("app.widgets.multiSelect.searchPlaceholder")}
-                                        notFoundMessage={translate("resources.terminals.notFoundMessage")}
-                                        isError={fieldState.invalid}
-                                        errorMessage={fieldState.error?.message}
-                                        disabled={terminalsLoadingProcess || !providerName}
-                                        modal
-                                    />
-                                </FormItem>
-                            )}
-                        />
-
-                        <FormField
-                            control={form.control}
-                            name="system_payment_instrument_code"
-                            render={({ field, fieldState }) => (
-                                <FormItem className="w-full p-2 sm:col-span-2">
-                                    <Label>
-                                        {translate(
-                                            "resources.paymentSettings.terminalPaymentInstruments.fields.system_payment_instrument_code"
-                                        )}
-                                    </Label>
-
-                                    <PopoverSelect
-                                        variants={systemPaymentInstrumentsData?.data || []}
-                                        value={systemPaymentInstrumentCode}
-                                        idField="code"
-                                        commandPlaceholder={translate("app.widgets.multiSelect.searchPlaceholder")}
-                                        setIdValue={field.onChange}
-                                        onChange={setSystemPaymentInstrumentCode}
-                                        variantKey="code"
-                                        notFoundMessage={translate(
-                                            "resources.paymentSettings.systemPaymentInstruments.notFoundMessage"
-                                        )}
-                                        isError={fieldState.invalid}
-                                        errorMessage={fieldState.error?.message}
-                                        disabled={systemPaymentInstrumentsDataLoading}
-                                        modal
-                                    />
-                                </FormItem>
-                            )}
-                        />
+                        <div className="w-full p-2 sm:col-span-2">
+                            <Input
+                                value={terminalPaymentInstrumentsData.system_payment_instrument_code}
+                                variant={InputTypes.GRAY}
+                                label={translate(
+                                    "resources.paymentSettings.terminalPaymentInstruments.fields.system_payment_instrument_code"
+                                )}
+                                disabled
+                            />
+                        </div>
                     </div>
 
                     <div className="grid grid-cols-1 sm:grid-cols-2">
@@ -459,8 +355,6 @@ export const TerminalPaymentInstrumentsEdit = ({ id, onClose = () => {} }: Termi
                                 hasErrors ||
                                 (!hasValid && form.watch("terminal_specific_parameters")?.length !== 0) ||
                                 !monacoEditorMounted ||
-                                providersLoadingProcess ||
-                                terminalsLoadingProcess ||
                                 submitButtonDisabled
                             }
                             type="submit"
