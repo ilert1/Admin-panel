2ebe647f3cd2821a0bf82281d9e81afda0ebf490
checkkick <tuskancik226@gmail.com>
2025-06-25 09:12:21 +0300

    fix: TableEditableCell remove z-index

--- source/src/components/widgets/shared/TableEditableCell.tsx
+++ source/src/components/widgets/shared/TableEditableCell.tsx
@@ -59,7 +59,7 @@ export function TableEditableCell<T>({
                         onChange={e => setValue(e.target.value)}
                     />
 
-                    <div className="z-10 flex flex-col items-center">
+                    <div className="flex flex-col items-center">
                         {isFetching ? (
                             <div className="flex items-center justify-center">
                                 <LoadingBalance className="h-6 w-6 overflow-hidden" />
