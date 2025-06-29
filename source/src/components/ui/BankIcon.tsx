28bf3dbf7bd3ea820029d3bd7ce35ab1e52e7295
Максим Сулин <zulus@Mac.lan>
2025-06-29 04:58:52 +0300

    fix: BankIcon with cache, correct on error hiding

--- source/src/components/ui/BankIcon.tsx
+++ source/src/components/ui/BankIcon.tsx
@@ -1,14 +1,32 @@
-import { useState } from "react";
-import { LoadingBalance } from "./loading";
+import { useQuery } from "@tanstack/react-query";
 
 interface BankIconProps {
     logoURL: string;
 }
 
+async function fetchImage(logoURL: string) {
+    const response = await fetch(logoURL);
+    if (!response.ok) {
+        return null;
+    }
+    return await response.blob();
+}
+
 export const BankIcon = ({ logoURL }: BankIconProps) => {
+    const { data: blob, error } = useQuery({
+        queryKey: ["image", logoURL],
+        queryFn: () => fetchImage(logoURL),
+        staleTime: Infinity, // Изображения никогда не устаревают
+        gcTime: 30 * 24 * 60 * 60 * 1000 // 30 дней в кеше
+        /* retry: 1,
+        retryDelay: 1000 */
+    });
+
+    const objectUrl = blob && URL.createObjectURL(blob);
+
     return (
         <div className="h-[24px] w-[24px]">
-            <img src={logoURL} loading="lazy" className={`h-full w-full object-contain`} />
+            {!error && objectUrl && <img src={objectUrl} className={`h-full w-full object-contain`} />}
         </div>
     );
 };
