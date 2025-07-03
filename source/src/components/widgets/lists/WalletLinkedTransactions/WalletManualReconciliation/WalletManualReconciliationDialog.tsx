a6b03b969aaf8a511687c7f624c60dd089bf3c58
checkkick <tuskancik226@gmail.com>
2025-07-03 15:42:30 +0300

    feat: MerchantSelect add for WalletManualReconciliation dialog

--- source/src/components/widgets/lists/WalletLinkedTransactions/WalletManualReconciliation/WalletManualReconciliationDialog.tsx
+++ source/src/components/widgets/lists/WalletLinkedTransactions/WalletManualReconciliation/WalletManualReconciliationDialog.tsx
@@ -3,9 +3,9 @@ import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } f
 import { Input } from "@/components/ui/Input/input";
 import { Label } from "@/components/ui/label";
 import { LoadingBalance } from "@/components/ui/loading";
-import { MerchantSelectFilter } from "../../../shared/MerchantSelectFilter";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { useWalletManualReconciliation } from "./useWalletManualReconciliation";
+import { MerchantSelect } from "@/components/widgets/components/Selects/MerchantSelect";
 
 interface IWalletManualReconciliationBar {
     open: boolean;
@@ -17,6 +17,10 @@ export const WalletManualReconciliationDialog = ({ open, onOpenChange }: IWallet
         translate,
         fiatShow,
         onFiatShowChanged,
+        merchantData,
+        merchantsLoadingProcess,
+        merchantValue,
+        setMerchantValue,
         merchantId,
         onMerchantChanged,
         merchantBalanceId,
@@ -55,7 +59,9 @@ export const WalletManualReconciliationDialog = ({ open, onOpenChange }: IWallet
                     />
 
                     <label
-                        onClick={() => onFiatShowChanged(!fiatShow)}
+                        onClick={() => {
+                            if (!isLoading) onFiatShowChanged(!fiatShow);
+                        }}
                         className="flex cursor-pointer items-center gap-2 self-start [&>*]:hover:border-green-20 [&>*]:active:border-green-50 [&_#checked]:hover:bg-green-20 [&_#checked]:active:bg-green-50">
                         <div className="relative flex h-4 w-4 items-center justify-center rounded-full border border-neutral-60 bg-white transition-all dark:bg-black">
                             {fiatShow && <div id="checked" className="h-2.5 w-2.5 rounded-full bg-green-50"></div>}
@@ -67,15 +73,19 @@ export const WalletManualReconciliationDialog = ({ open, onOpenChange }: IWallet
 
                     {fiatShow && (
                         <div className="flex flex-col flex-wrap justify-between gap-2 sm:flex-row sm:items-end sm:gap-x-4 sm:gap-y-3">
-                            <div className="flex flex-1 flex-col gap-1 md:gap-1">
+                            <div className="flex flex-1 flex-col gap-1 md:min-w-72 md:gap-1">
                                 <Label className="mb-0 md:text-nowrap" variant="note-1">
                                     {translate("resources.transactions.filter.filterByAccount")}
                                 </Label>
 
-                                <MerchantSelectFilter
-                                    merchant={merchantId}
-                                    onMerchantChanged={onMerchantChanged}
-                                    resource="merchant"
+                                <MerchantSelect
+                                    merchants={merchantData || []}
+                                    value={merchantValue}
+                                    onChange={setMerchantValue}
+                                    setIdValue={onMerchantChanged}
+                                    disabled={merchantsLoadingProcess}
+                                    style="Black"
+                                    modal
                                 />
                             </div>
 
