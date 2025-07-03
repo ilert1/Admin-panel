f5e13e5df0c5748e9e461335d11c4dfbb9c1ca4d
checkkick <tuskancik226@gmail.com>
2025-07-03 11:19:30 +0300

    feat: create new MerchantSelect component

---
+++ source/src/components/widgets/components/Selects/MerchantSelect.tsx
@@ -0,0 +1,43 @@
+import { Merchant } from "@/api/enigma/blowFishEnigmaAPIService.schemas";
+import { IPopoverSelect, PopoverSelect } from "./PopoverSelect";
+import { useTranslate } from "react-admin";
+
+interface MerchantSelectProps extends IPopoverSelect {
+    merchants: Merchant[];
+}
+
+export const MerchantSelect = ({
+    merchants,
+    value,
+    onChange,
+    setIdValue,
+    isError,
+    style = "Grey",
+    errorMessage,
+    disabled,
+    modal,
+    placeholder,
+    isLoading
+}: MerchantSelectProps) => {
+    const translate = useTranslate();
+
+    return (
+        <PopoverSelect
+            variants={merchants}
+            value={value}
+            onChange={onChange}
+            variantKey="name"
+            idField="id"
+            setIdValue={setIdValue}
+            commandPlaceholder={translate("app.widgets.multiSelect.searchPlaceholder")}
+            notFoundMessage={translate("resources.merchant.notFoundMessage")}
+            isError={isError}
+            style={style}
+            errorMessage={errorMessage}
+            disabled={disabled}
+            placeholder={placeholder || translate("resources.merchant.selectPlaceholder")}
+            modal={modal}
+            isLoading={isLoading}
+        />
+    );
+};
