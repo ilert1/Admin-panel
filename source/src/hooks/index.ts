602cc86cbd19013c354a725b70ff48b2ef96ac4d
Konstantin <tuskancik226@gmail.com>
2025-07-05 13:56:22 +0300

    fix: remove hook useFetchDataForDirections

--- source/src/hooks/index.ts
+++ source/src/hooks/index.ts
@@ -1,5 +1,4 @@
 export { useTransactionActions } from "../components/widgets/show/Transaction/useTransactionActions";
-export { useFetchDataForDirections } from "./useFetchDataForDirections";
 export { usePreventFocus } from "./usePreventFocus";
 export { useGetTransactionState } from "./getTransactionState";
 export { useFetchMerchants } from "./useFetchMerchants";
