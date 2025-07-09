0f0a7fcd14cdb1314b119794c69771939bdddc4f
checkkick <tuskancik226@gmail.com>
2025-07-09 11:20:13 +0300

    fix: TransactionShow enabled state_update table

--- source/src/components/widgets/show/Transaction/TransactionShow.tsx
+++ source/src/components/widgets/show/Transaction/TransactionShow.tsx
@@ -1,5 +1,5 @@
-import { useTranslate, useGetManyReference, usePermissions } from "react-admin";
-import { SimpleTable } from "@/components/widgets/shared";
+import { useTranslate, useGetManyReference, usePermissions, ListContextProvider } from "react-admin";
+import { DataTable, SimpleTable } from "@/components/widgets/shared";
 import { TextField } from "@/components/ui/text-field";
 import { useCallback, useMemo, useState } from "react";
 import { LoadingBlock } from "@/components/ui/loading";
@@ -23,6 +23,7 @@ import { useAbortableShowController } from "@/hooks/useAbortableShowController";
 import { useSheets } from "@/components/providers/SheetProvider";
 import { Merchant } from "@/api/enigma/blowFishEnigmaAPIService.schemas";
 import { useAppToast } from "@/components/ui/toast/useAppToast";
+import { useAbortableListController } from "@/hooks/useAbortableListController";
 
 interface TransactionShowProps {
     id: string;
@@ -47,14 +48,14 @@ export const TransactionShow = ({ id }: TransactionShowProps) => {
         }
     });
 
-    // const listContext = useAbortableListController<Transaction.TransactionStateUpdate>({
-    //     resource: "state_update",
-    //     filter: { id },
-    //     disableSyncWithLocation: true,
-    //     queryOptions: {
-    //         enabled: permissions === "admin"
-    //     }
-    // });
+    const listContext = useAbortableListController<Transaction.TransactionStateUpdate>({
+        resource: "state_update",
+        filter: { id },
+        disableSyncWithLocation: true,
+        queryOptions: {
+            enabled: permissions === "admin"
+        }
+    });
 
     const [newState, setNewState] = useState("");
     const [dialogOpen, setDialogOpen] = useState(false);
@@ -73,7 +74,7 @@ export const TransactionShow = ({ id }: TransactionShowProps) => {
         sendWebhookLoading
     } = useTransactionActions(data, context.record);
 
-    const { feesColumns, briefHistory /* stateUpdateColumns */ } = useGetTransactionShowColumns();
+    const { feesColumns, briefHistory, stateUpdateColumns } = useGetTransactionShowColumns();
 
     const trnId = useMemo<string>(() => context.record?.id || "", [context]);
 
@@ -319,7 +320,7 @@ export const TransactionShow = ({ id }: TransactionShowProps) => {
                 </div>
             )}
 
-            {/* {permissions === "admin" && (
+            {permissions === "admin" && (
                 <div className="flex flex-col gap-2">
                     <span>{translate("resources.transactions.stateUpdate.title")}</span>
 
@@ -327,7 +328,7 @@ export const TransactionShow = ({ id }: TransactionShowProps) => {
                         <DataTable columns={stateUpdateColumns} />
                     </ListContextProvider>
                 </div>
-            )} */}
+            )}
         </div>
     );
 };
