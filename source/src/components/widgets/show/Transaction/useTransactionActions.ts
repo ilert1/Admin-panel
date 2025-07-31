009652d232e59f9047480937ec2a3e46eb720cef
Konstantin <tuskancik226@gmail.com>
2025-07-31 12:32:13 +0300

    fix: trnsaction status changes translate

--- source/src/components/widgets/show/Transaction/useTransactionActions.ts
+++ source/src/components/widgets/show/Transaction/useTransactionActions.ts
@@ -57,9 +57,9 @@ export const useTransactionActions = (
                             const currentState = states.find(item => item.state_int === state);
                             appToast(
                                 "success",
-                                `${translate("resources.transactions.fields.state.state_changed")} ${
-                                    currentState?.state_description
-                                }`
+                                `${translate("resources.transactions.fields.state.state_changed")} ${translate(
+                                    `resources.transactions.states.${currentState?.state_description?.toLowerCase()}`
+                                )}`
                             );
                         } else {
                             throw new Error(json.error || "Unknown error");
