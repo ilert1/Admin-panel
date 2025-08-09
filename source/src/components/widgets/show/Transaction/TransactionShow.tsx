3035f8c0d38de255cd2fa8066dda26336c9e7a00
Максим Сулин <zulus@MacBook-Pro-Maksim.local>
2025-08-09 12:37:34 +0300

    removed console.log

--- source/src/components/widgets/show/Transaction/TransactionShow.tsx
+++ source/src/components/widgets/show/Transaction/TransactionShow.tsx
@@ -129,9 +129,6 @@ export const TransactionShow = ({ id }: TransactionShowProps) => {
         context.record?.destination?.id
     );
 
-    console.log("context.record", context.record);
-    console.log("data?.transactionTypes", data?.transactionTypes);
-
     return (
         <div className="top-[82px] flex h-full flex-col gap-6 overflow-auto p-4 pt-0 md:px-[42px]">
             {permissions === "admin" && (
