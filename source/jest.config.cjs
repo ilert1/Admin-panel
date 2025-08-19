e1341a0f2206a834d4b9202dfec14f28f73c2ebe
checkkick <tuskancik226@gmail.com>
2025-08-19 17:20:13 +0300

    feat: add REact TestingLibrary for UI tests

--- source/jest.config.cjs
+++ source/jest.config.cjs
@@ -5,9 +5,13 @@ module.exports = {
     transform: {
         "^.+\\.(ts|tsx)$": "ts-jest"
     },
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // важно!
     testMatch: ["<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}", "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}"],
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts", "!src/**/index.ts", "!src/**/types.ts"],
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1"
+    },
     coverageThreshold: {
         global: {
             branches: 80,
