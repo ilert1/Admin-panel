3bf20376d9b3662a174db821eb36ac2e092f1cb8
checkkick <tuskancik226@gmail.com>
2025-08-20 10:54:40 +0300

    fix: jest.config clear unused params

--- source/jest.config.ts
+++ source/jest.config.ts
@@ -11,11 +11,6 @@ export default async (): Promise<Config> => {
             "^.+\\.(js|jsx)$": "babel-jest"
         },
         setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-        testMatch: [
-            "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
-            "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}"
-        ],
-        moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
         collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.d.ts", "!src/**/index.ts", "!src/**/types.ts"],
         coverageThreshold: {
             global: { branches: 80, functions: 80, lines: 80, statements: 80 }
