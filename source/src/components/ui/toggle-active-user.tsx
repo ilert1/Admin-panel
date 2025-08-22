b1db71431b8b2153e71667316f0f97e6aa841e41
checkkick <tuskancik226@gmail.com>
2025-08-22 15:52:08 +0300

    feat: add small tests for toggle-active-user

--- source/src/components/ui/toggle-active-user.tsx
+++ source/src/components/ui/toggle-active-user.tsx
@@ -7,9 +7,9 @@ export const ToggleActiveUser = ({ active }: { active: boolean }) => {
             <div
                 className={clsx("flex items-center justify-center rounded-full bg-neutral-0 p-1", active && "ml-auto")}>
                 {active ? (
-                    <LockKeyholeOpen size={15} className="text-green-50" />
+                    <LockKeyholeOpen data-testid="lock-keyhole-open" size={15} className="text-green-50" />
                 ) : (
-                    <LockKeyhole size={15} className="text-red-40" />
+                    <LockKeyhole data-testid="lock-keyhole" size={15} className="text-red-40" />
                 )}
             </div>
         </div>
