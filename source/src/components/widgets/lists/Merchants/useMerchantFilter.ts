8981fa58a581c1ff450d97f3203393e7384e18e3
checkkick <tuskancik226@gmail.com>
2025-09-19 11:52:07 +0300

    feat: MerchantsFilter add useEffect for filter by provider and terminal

--- source/src/components/widgets/lists/Merchants/useMerchantFilter.ts
+++ source/src/components/widgets/lists/Merchants/useMerchantFilter.ts
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { useListContext, useTranslate } from "react-admin";
 
 const useMerchantFilter = () => {
-    const { filterValues, setFilters, displayedFilters, setPage } = useListContext();
+    const { filterValues, setFilters, displayedFilters, setPage, total } = useListContext();
     const { merchantData, merchantsLoadingProcess } = useMerchantsListWithoutPagination();
     const translate = useTranslate();
 
@@ -12,11 +12,26 @@ const useMerchantFilter = () => {
     const [merchantValue, setMerchantValue] = useState("");
 
     useEffect(() => {
-        if (merchantData) {
-            setMerchantValue(merchantData?.find(merchant => merchant.id === filterValues?.id)?.name || "");
+        if (merchantData && filterValues?.id) {
+            const foundMerchant = merchantData?.find(merchant => merchant.id === filterValues?.id)?.name;
+
+            if (foundMerchant) {
+                setMerchantValue(foundMerchant);
+            } else {
+                setFilters({}, displayedFilters, true);
+                setMerchantValue("");
+            }
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [merchantData]);
+
+    useEffect(() => {
+        if (total === 0) {
+            setFilters({}, displayedFilters, true);
+            setMerchantValue("");
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [total]);
 
     const onPropertySelected = debounce((value: string, type: "id") => {
         if (value) {
