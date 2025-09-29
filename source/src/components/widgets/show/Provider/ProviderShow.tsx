1464f92aecb8e5f5dc8cd1ed26d97d5613f35858
David <email@mail.com>
2025-09-29 10:32:57 +0400

    fix: removed redundant classname

--- source/src/components/widgets/show/Provider/ProviderShow.tsx
+++ source/src/components/widgets/show/Provider/ProviderShow.tsx
@@ -184,7 +184,7 @@ export const ProviderShow = ({ id, onOpenChange }: ProviderShowProps) => {
                         <div className="max-w-auto flex flex-wrap gap-2">
                             {context.record.payment_types && context.record.payment_types?.length > 0 ? (
                                 context.record.payment_types.map(pt => {
-                                    return <PaymentTypeIcon key={pt.code} type={pt.code} className="h-7 w-7" />;
+                                    return <PaymentTypeIcon key={pt.code} type={pt.code} />;
                                 })
                             ) : (
                                 <span className="title-1">-</span>
